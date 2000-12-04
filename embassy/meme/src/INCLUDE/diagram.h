084de6df120e5145e660f2678972839c3ad0bc3f
ajb <ajb>
2000-12-04 11:21:04 +0000

    missvar

--- embassy/meme/src/INCLUDE/diagram.h
+++ embassy/meme/src/INCLUDE/diagram.h
@@ -6,9 +6,16 @@
 #include "macros.h"
 #include "logodds.h"
 
-DEXTERN(int, norder, 0);/* number of motifs for which order is given */
-EXTERN int order[MAXLO];/* left-to-right order of motifs */
-EXTERN int space[MAXLO];/* distance from end of previous motif to start this */
+int dptr;
+int order[MAXLO];
+int space[MAXLO];
+int norder;
+char diagram[MAXLO];
+
+
+/*DEXTERN(int, norder, 0);*//* number of motifs for which order is given */
+/*EXTERN int order[MAXLO];*//* left-to-right order of motifs */
+/*EXTERN int space[MAXLO];*//* distance from end of previous motif to start this */
 
 /* motif block diagram in mast format (see diagram.y):
         diagram         <- mdiagram | sdiagram
@@ -17,8 +24,10 @@ EXTERN int space[MAXLO];/* distance from end of previous motif to start this */
         motif           <- [integer]
         spacer          <- integer
 */
-DEXTERN(char *, diagram, (char *)0);	
-EXTERN int dptr;	/* current read position in diagram */
+/*DEXTERN(char *, diagram, (char *)0);	*/
+/*EXTERN int dptr;*/	/* current read position in diagram */
+
+
 
 /* prevent compiler complaints */
 #if defined(__STDC__) || defined(__cplusplus)
