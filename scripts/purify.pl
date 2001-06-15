81de273c65c137e5f1e533c1a6f78921b4ba55c8
rice <rice>
2001-06-15 16:31:16 +0000

    Purify test cases

---
+++ scripts/purify.pl
@@ -0,0 +1,39 @@
+#!/usr/local/bin/perl -w
+
+%tests = (
+	  "feat1" => "seqretallfeat -auto tembl:hsfau",
+	  "featmany" => "seqretallfeat -auto 'tembl:hsf*'",
+	  "embl1" => "seqret -auto tembl:hsfau",
+	  "emblmany" => "seqret -auto 'tembl:hsf*'",
+	  "" => "",
+);
+
+if (defined($ENV{EPURE})) {
+  $purepath = "$ENV{EPURE}/";
+}
+else {
+  $purepath = "";
+}
+
+@dotest = @ARGV;
+
+foreach $name (@dotest) {
+  print "Test $name\n";
+
+  if (defined($tests{$name})) {
+  print "Running $purepath$tests{$name}\n";
+    $status = system ("$purepath$tests{$name}");
+    
+    if ($status) {
+      print STDERR "Purify test $name returned status $status\n";
+    }
+    else {
+      print STDERR "Purify test $name OK\n";
+    }
+  }
+  else {
+      print STDERR "ERROR: Unknown test $name \n";
+  }
+}
+
+exit();
