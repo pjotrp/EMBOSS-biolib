65a3d4388c5e95660491c526b8cfefcf0f9d8590
rice <rice>
2001-09-20 13:06:34 +0000

    seqretsingle to test reading and writing of a single sequence (e.g. for new USA syntax testing)

---
+++ emboss/seqretsingle.c
@@ -0,0 +1,27 @@
+#include "emboss.h"
+
+
+/* @prog seqret ***************************************************************
+**
+** Reads and writes (returns) sequences
+**
+******************************************************************************/
+
+int main(int argc, char **argv)
+{
+
+    AjPSeqout seqout;
+    AjPSeq seq = NULL;
+    AjBool firstonly;
+
+    embInit ("seqretsingle", argc, argv);
+
+    seqout = ajAcdGetSeqoutall ("outseq");
+    seq = ajAcdGetSeq ("sequence");
+
+    ajSeqWrite (seqout, seq);
+    ajSeqWriteClose (seqout);
+
+    ajExit ();
+    return 0;
+}
