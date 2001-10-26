ecffc94f7b0da5a5e3ea3ac590fc520990bedf61
ajb <ajb>
2001-10-26 08:29:45 +0000

    added seqset functions

--- ajax/core/ajjava.c
+++ ajax/core/ajjava.c
@@ -6,11 +6,13 @@
 #define AJNOTFOUND -999
 
 static AjBool ajJavaGetSeqFromUsa (AjPStr thys, AjPSeq *seq);
+static AjBool ajJavaGetSeqsetFromUsa (AjPStr thys, AjPSeqset *seq);
 
 
 JNIEXPORT jboolean JNICALL Java_org_emboss_jemboss_parser_Ajax_seqType
            (JNIEnv *env, jobject obj, jstring usa)
 {
+
     AjPStr name=NULL;
     char *javaname=NULL;
     AjBool ok=ajFalse;
@@ -54,6 +56,51 @@ JNIEXPORT jboolean JNICALL Java_org_emboss_jemboss_parser_Ajax_seqType
 
 
 
+JNIEXPORT jboolean JNICALL Java_org_emboss_jemboss_parser_Ajax_seqsetType
+           (JNIEnv *env, jobject obj, jstring usa)
+{
+    AjPStr name=NULL;
+    char *javaname=NULL;
+    AjBool ok=ajFalse;
+    AjPSeqset seq=NULL;
+    AjBool nuc=ajFalse;
+    ajint  len=0;
+    float  weight=0.;
+    jclass jvc = (*env)->GetObjectClass(env,obj);
+    jfieldID field;
+    
+    name = ajStrNew();
+    seq  = ajSeqsetNew();
+    
+    javaname = (char *) (*env)->GetStringUTFChars(env,usa,0);
+    ajStrAssC(&name,javaname);
+    (*env)->ReleaseStringUTFChars(env,usa,javaname);
+
+    ok = ajJavaGetSeqsetFromUsa(name,&seq);
+    if(ok)
+    {
+	len = ajSeqsetLen(seq);
+	nuc = ajSeqsetIsNuc(seq);
+	weight = ajSeqsetTotweight(seq);
+	
+	field = (*env)->GetStaticFieldID(env,jvc,"length","I");
+	(*env)->SetStaticIntField(env,jvc,field,len);
+
+	field = (*env)->GetStaticFieldID(env,jvc,"protein","Z");
+	(*env)->SetStaticBooleanField(env,jvc,field,(unsigned char)!nuc);
+
+	field = (*env)->GetStaticFieldID(env,jvc,"weight","F");
+	(*env)->SetStaticFloatField(env,jvc,field,weight);
+    }
+    
+    ajStrDel(&name);
+    /* Needs an ajSeqsetDel here, but unwritten so far. Will do. AJB */
+
+    return (unsigned char) ok;
+}
+
+
+
 
 
 static AjBool ajJavaGetSeqFromUsa (AjPStr thys, AjPSeq *seq)
@@ -76,6 +123,31 @@ static AjBool ajJavaGetSeqFromUsa (AjPStr thys, AjPSeq *seq)
 
     return ajTrue;
 }
+
+
+
+static AjBool ajJavaGetSeqsetFromUsa (AjPStr thys, AjPSeqset *seq)
+{
+    AjPSeqin seqin;
+    AjBool ok;
+  
+    ajNamInit("emboss");
+  
+    seqin = ajSeqinNew();
+    seqin->multi = ajTrue;
+    seqin->Text  = ajFalse;
+  
+    ajSeqinUsa (&seqin, thys);
+    ok = ajSeqsetRead(*seq, seqin);
+    ajSeqinDel (&seqin);
+
+
+    if(!ok)
+	return ajFalse;
+
+    return ajTrue;
+}
+
 
 
 #endif
--- ajax/core/ajjava.h
+++ ajax/core/ajjava.h
@@ -11,7 +11,10 @@ extern "C" {
 #endif
 
 JNIEXPORT jboolean JNICALL Java_org_emboss_jemboss_parser_Ajax_seqType 
-  (JNIEnv *, jobject, jstring);
+    (JNIEnv *, jobject, jstring);
+JNIEXPORT jboolean JNICALL Java_org_emboss_jemboss_parser_Ajax_seqsetType
+    (JNIEnv *env, jobject obj, jstring usa);
+
 
 #ifdef __cplusplus
 }
