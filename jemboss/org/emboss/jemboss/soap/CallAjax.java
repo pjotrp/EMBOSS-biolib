ee41881224bc5e2133abb6eb6a8d8dfb9acf7899
timc <timc>
2001-11-12 13:34:31 +0000

    call EmbreoPublicRequest

--- jemboss/org/emboss/jemboss/soap/CallAjax.java
+++ jemboss/org/emboss/jemboss/soap/CallAjax.java
@@ -20,20 +20,8 @@ package org.emboss.jemboss.soap;
 
 import java.io.*;
 import java.util.*;
-
-import uk.ac.mrc.hgmp.embreo.*;
-
-import java.net.*;
-import org.w3c.dom.*;
-import org.xml.sax.*;
-import javax.xml.parsers.*;
-import javax.mail.*;
-import org.apache.soap.util.xml.*;
-import org.apache.soap.*;
-import org.apache.soap.encoding.*;
-import org.apache.soap.encoding.soapenc.*;
 import org.apache.soap.rpc.*;
-import org.apache.soap.transport.http.SOAPHTTPConnection;
+import uk.ac.mrc.hgmp.embreo.*;
 
 public class CallAjax {
 
@@ -42,116 +30,43 @@ public class CallAjax {
   private int length;
   private float weight;
   private boolean protein;
+  private EmbreoPublicRequest epr;
 
-   public CallAjax(String fileContent, String seqtype, EmbreoParams mysettings) throws EmbreoAuthException {
-
-     URL proglisturl;
-     try {
-       proglisturl = new URL(mysettings.getPublicSoapURL());
-     } catch (Exception e) { 
-       System.err.println("While Initialising URL Caught Exception (" +
-			  e.getMessage ());
-       return;
-     }
-     int i;
-
-     System.out.println("Settings URL: "+mysettings.getPrivateSoapURL());
-
-     SOAPHTTPConnection proglistconn = new SOAPHTTPConnection();
-     //
-     // if proxy, set the proxy values
-     //
-     if (mysettings.getUseProxy() == true) {
-       if (mysettings.getProxyHost() != null) {
-	 proglistconn.setProxyHost(mysettings.getProxyHost());
-	 proglistconn.setProxyPort(mysettings.getProxyPortNum());
-       }
-     }
-     // if authenticated, add the authentication headers
-     // must set them to the right values first
-     //
-     if (mysettings.getUseAuth() == true) {
-       if (mysettings.getServiceUserName() != null) {
-	 if (mysettings.getServicePasswd() != null) {
-	   proglistconn.setUserName(mysettings.getServiceUserName());
-	   proglistconn.setPassword(mysettings.getServicePasswd());
-	 }
-       }
-     }
-     Call proglistcall = new Call();
-     proglistcall.setSOAPTransport(proglistconn);
-     proglistcall.setTargetObjectURI(mysettings.getPublicSoapService());
-     proglistcall.setMethodName("call_ajax");
-     proglistcall.setEncodingStyleURI(Constants.NS_URI_SOAP_ENC);
+   public CallAjax(String fileContent, String seqtype, EmbreoParams mysettings) 
+       throws EmbreoAuthException 
+   {
 
      Vector params = new Vector();
      params.addElement(new Parameter("fileContent", String.class,
                                     fileContent, null));
      params.addElement(new Parameter("seqtype", String.class,
                                      seqtype, null));
-     proglistcall.setParams(params);
 
-     Response proglistresp;
-     try 
+     epr = new EmbreoPublicRequest(mysettings,"call_ajax",params);
+
+     Hashtable res = epr.getHash();
+     Enumeration enumRes = res.keys();
+     while(enumRes.hasMoreElements()) 
      {
-       proglistresp = proglistcall.invoke(proglisturl,null);
-     } 
-     catch(SOAPException e) 
-     {
-       System.err.println("Caught SOAPException (" +
-			  e.getFaultCode () + "): " +
-			  e.getMessage ());
-       Hashtable efaulth = proglistconn.getHeaders();
-       if(efaulth.containsKey("WWW-Authenticate")) 
-       {
-	 System.err.println("Auth header found!");
-	 throw new EmbreoAuthException("Authentication Failed");
-       }
-       return;
+       String thiskey = (String)enumRes.nextElement().toString();
+       if(thiskey.equals("length"))
+         length = ((Integer)res.get(thiskey)).intValue();
+       if(thiskey.equals("weight"))
+         weight = ((Float)res.get(thiskey)).floatValue();
+       if(thiskey.equals("protein"))
+         protein = ((Boolean)res.get(thiskey)).booleanValue();
      }
-
-    //Check response
-     if (!proglistresp.generatedFault()) 
-     {
-       Parameter progret = proglistresp.getReturnValue();
-       Object progvalue = progret.getValue();
-      
-       Vector vans = (Vector)progvalue;
-       int n = vans.size();
-       for(int j=0;j<n;j+=2)
-       {
-        if(vans.get(j).equals("length"))
-          length = ((Integer)(vans.get(j+1))).intValue();
-        else if(vans.get(j).equals("weight"))
-          weight = ((Float)(vans.get(j+1))).floatValue();
-        else if(vans.get(j).equals("protein"))
-           protein = ((Boolean)(vans.get(j+1))).booleanValue();
-        else if(vans.get(j).equals("status"))
-          status = (String)(vans.get(j+1));
-       }
-       System.out.println("length/weight/protein " + length + " " + weight );
-      
-     } 
-     else 
-     {
-       Fault fault = proglistresp.getFault();
-       System.err.println("Generated fault: ");
-       System.out.println("  Fault Code   = " + fault.getFaultCode());
-       System.out.println("  Fault String = " + fault.getFaultString());
-     }
-
-
    }
 
 
   public String getStatus() 
   {
-    return status;
+    return epr.getVal("status");
   }
   
   public String getStatusMsg() 
   {
-    return statusmsg;
+    return epr.getVal("msg");
   }
 
   public int getLength()
--- jemboss/org/emboss/jemboss/soap/GetWossname.java
+++ jemboss/org/emboss/jemboss/soap/GetWossname.java
@@ -21,129 +21,21 @@ package org.emboss.jemboss.soap;
 import java.io.*;
 import java.util.*;
 
-import uk.ac.mrc.hgmp.embreo.EmbreoParams;
+import uk.ac.mrc.hgmp.embreo.*;
 
-//PCT SOAP add
-import java.net.*;
-import org.w3c.dom.*;
-import org.xml.sax.*;
-import javax.xml.parsers.*;
-import javax.mail.*;
-import org.apache.soap.util.xml.*;
-import org.apache.soap.*;
-import org.apache.soap.encoding.*;
-import org.apache.soap.encoding.soapenc.*;
-import org.apache.soap.rpc.*;
-import org.apache.soap.transport.http.SOAPHTTPConnection;
 
 public class GetWossname 
 {
 
-  private String statusmsg;
-  private String status;
+   private String statusmsg;
+   private String status;
    private String dbText;
+   private EmbreoPublicRequest epr;
 
    public GetWossname(EmbreoParams mysettings)
    {
-     URL proglisturl;
-     try {
-       proglisturl = new URL(mysettings.getPublicSoapURL());
-     } catch (Exception e) { 
-       System.err.println("While Initialising URL Caught Exception (" +
-			  e.getMessage ());
-       return;
-     }
-     int i;
 
-     SOAPHTTPConnection proglistconn = new SOAPHTTPConnection();
-     //
-     // if proxy, set the proxy values
-     //
-     if (mysettings.getUseProxy() == true) {
-       if (mysettings.getProxyHost() != null) {
-	 proglistconn.setProxyHost(mysettings.getProxyHost());
-	 proglistconn.setProxyPort(mysettings.getProxyPortNum());
-       }
-     }
-     //
-     // end proxy handling
-     //
-     Call proglistcall = new Call();
-     proglistcall.setSOAPTransport(proglistconn);
-     proglistcall.setTargetObjectURI(mysettings.getPublicSoapService());
-     proglistcall.setMethodName("getWossname");
-     proglistcall.setEncodingStyleURI(Constants.NS_URI_SOAP_ENC);
-
-     Response proglistresp;
-     try {
-       proglistresp = proglistcall.invoke(proglisturl,null);
-     } catch(SOAPException e) {
-       System.err.println("Caught SOAPException (" +
-			  e.getFaultCode () + "): " +
-			  e.getMessage ());
-       return;
-     }
-    //Check response
-    if (!proglistresp.generatedFault()) {
-      Parameter progret = proglistresp.getReturnValue();
-      Object progvalue = progret.getValue();
-      
-      //
-      // should at this point check the application status we get
-      // back
-      //
-
-      Vector progans = proglistresp.getParams();
-      Parameter progansp;
-      dbText = "";
-      String tstr;
-      if (progans != null) {
-        progansp = (Parameter) progans.get(0);
-        int progians = progans.size();
-        tstr = (String)progvalue;
-        if (tstr.equalsIgnoreCase("status"))
-	  {status=progansp.getValue().toString();}
-        if (tstr.equalsIgnoreCase("msg"))
-	  {statusmsg=progansp.getValue().toString();}
-        if (tstr.equalsIgnoreCase("showdb"))
-	  {dbText=progansp.getValue().toString();}
-        for (int j=1; j<progians;j++) {
-	  Parameter progansk = (Parameter) progans.get(j);
-	  tstr = progansk.getValue().toString();
-	  j++;
-	  Parameter progansv = (Parameter) progans.get(j);
-	  if (tstr.equalsIgnoreCase("status"))
-	    {status=progansv.getValue().toString();}
-	  if (tstr.equalsIgnoreCase("msg"))
-	    {statusmsg=progansv.getValue().toString();}
-	  if (tstr.equalsIgnoreCase("showdb"))
-	    {dbText=progansv.getValue().toString();}
-        }
-      }
-      else
-      {
-        Vector vans = (Vector)progvalue;
-        int n = vans.size();
-        for(int j=0;j<n;j+=2)
-        {
-          if(((String)vans.get(j)).equalsIgnoreCase("wossname"))
-            dbText=(String)vans.get(j+1);
-          else if(((String)vans.get(j)).equalsIgnoreCase("msg"))
-            statusmsg=(String)vans.get(j+1);
-          else if(((String)vans.get(j)).equalsIgnoreCase("status"))
-            status=(String)vans.get(j+1);
-        }
-      }
-
-
-    }
-    else 
-    {
-      Fault fault = proglistresp.getFault();
-      System.err.println("Generated fault: ");
-      System.out.println("  Fault Code   = " + fault.getFaultCode());
-      System.out.println("  Fault String = " + fault.getFaultString());
-    }
+     epr = new EmbreoPublicRequest(mysettings,"getWossname");
 
    }
 
@@ -159,7 +51,7 @@ public class GetWossname
 
    public String getDBText() 
    {
-     return dbText;
+     return epr.getVal("wossname");
    }
 
 }
