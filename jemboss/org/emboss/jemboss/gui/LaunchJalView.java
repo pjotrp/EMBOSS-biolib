1c63cc96922b177083e24aa7c9e6225a0535e43c
timc <timc>
2001-11-22 14:34:35 +0000

    LaunchJalView

---
+++ jemboss/org/emboss/jemboss/gui/LaunchJalView.java
@@ -0,0 +1,121 @@
+/********************************************************************
+*
+*  This library is free software; you can redistribute it and/or
+*  modify it under the terms of the GNU Library General Public
+*  License as published by the Free Software Foundation; either
+*  version 2 of the License, or (at your option) any later version.
+*
+*  This library is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+*  Library General Public License for more details.
+*
+*  You should have received a copy of the GNU Library General Public
+*  License along with this library; if not, write to the
+*  Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+*  Boston, MA  02111-1307, USA.
+*
+*  @author: Copyright (C) Tim Carver
+*
+********************************************************************/
+
+
+package org.emboss.jemboss.gui;
+
+import java.awt.*;
+import java.io.*;
+import javax.swing.*;
+import javax.swing.event.*;
+import java.awt.event.*;
+import org.emboss.jemboss.gui.form.TextFieldSink;
+
+
+public class LaunchJalView extends JFrame
+{
+
+  public LaunchJalView()
+  {
+    super("Jalview ");
+
+    final TextFieldSink tfs = new TextFieldSink();
+
+    Box bacross = Box.createHorizontalBox();
+    Box bdown = Box.createVerticalBox();
+    bdown.add(Box.createVerticalStrut(5));
+
+    JLabel lname = new JLabel("Multiple Sequence Filename");
+    lname.setFont(org.emboss.jemboss.gui.form.SectionPanel.labfont);
+    lname.setForeground(Color.black);
+    bdown.add(Box.createVerticalStrut(1));
+    bacross.add(Box.createHorizontalStrut(1));
+    bacross.add(lname);
+    bacross.add(Box.createHorizontalGlue());
+    bdown.add(bacross);
+ 
+    bdown.add(Box.createVerticalStrut(1));
+
+    bacross = Box.createHorizontalBox();
+    bacross.add(Box.createHorizontalStrut(1));
+    bacross.add(tfs);
+    bacross.add(Box.createHorizontalGlue());
+    bdown.add(bacross);
+
+    bdown.add(Box.createVerticalStrut(5));
+    
+    lname = new JLabel("File Format");
+    lname.setFont(org.emboss.jemboss.gui.form.SectionPanel.labfont);
+    lname.setForeground(Color.black);
+    bacross = Box.createHorizontalBox();
+    bacross.add(Box.createHorizontalStrut(1));
+    bacross.add(lname);
+    bacross.add(Box.createHorizontalGlue());
+    bdown.add(bacross);
+
+    bdown.add(Box.createVerticalStrut(1));
+    String sformats[] = {"MSF", "FASTA", "CLUSTAL", "PIR"};
+    final JComboBox format = new JComboBox(sformats);
+    bdown.add(format);
+    bdown.add(Box.createVerticalStrut(5));
+
+    JButton launch = new JButton("LAUNCH");
+    bacross = Box.createHorizontalBox();
+    bacross.add(Box.createHorizontalStrut(1));
+    bacross.add(launch);
+    bacross.add(Box.createHorizontalGlue());
+    bdown.add(bacross);
+
+    launch.addActionListener(new ActionListener()
+    {
+      public void actionPerformed(ActionEvent e)
+      {
+        String ps = System.getProperty("path.separator");
+        String fs = System.getProperty("file.separator");
+        String launchCommand = System.getProperty("java.home") + fs + "bin"
+                                          + fs +"java jalview.AlignFrame ";
+
+        launchCommand = launchCommand.concat(tfs.getText() + " File ");
+        launchCommand = launchCommand.concat((String)format.getSelectedItem()); 
+
+        Runtime jalviewRun = Runtime.getRuntime();
+        try
+        {
+          Process p = jalviewRun.exec(launchCommand);
+          setVisible(false);
+        }
+        catch(IOException ioe)
+        {
+          System.out.println("Failed to launch jalview " + launchCommand);
+        }
+        
+      }
+    });
+
+    JPanel jp = (JPanel)getContentPane();
+    jp.setLayout(new BorderLayout());  
+    jp.add(bdown, BorderLayout.CENTER);
+    pack();
+    setVisible(true);      
+  }
+
+}
+
