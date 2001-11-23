48b7e627e8cd65d00884d8aab3152ae79e26bf78
gwilliam <gwilliam>
2001-11-23 14:06:01 +0000

    trivial doc change

--- emboss/primer3.c
+++ emboss/primer3.c
@@ -1,5 +1,5 @@
 /* @source primer3 application
-** Picks primers for PCR reactions
+** Picks PCR primers and hybridization oligos
 ** 
 ** @author: Copyright (C) Gary Williams (gwilliam@hgmp.mrc.ac.uk)
 ** 5 Nov 2001 - GWW - written
