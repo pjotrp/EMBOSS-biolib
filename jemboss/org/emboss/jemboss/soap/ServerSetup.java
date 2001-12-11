6ee80d299d53fb96d95d889936afc9f4b7a526fc
timc <timc>
2001-12-11 13:35:40 +0000

    layout adjusted

--- jemboss/org/emboss/jemboss/soap/ServerSetup.java
+++ jemboss/org/emboss/jemboss/soap/ServerSetup.java
@@ -39,8 +39,16 @@ public class ServerSetup extends JPanel
   private MemoryComboBox publicName;
   private MemoryComboBox privateName;
 
+  private JCheckBox userAuth;
+
   private EmbreoParams mysettings; 
 
+/**
+*
+* Setting for public and private settings panel.
+* @param EmbreoParams SOAP parameters
+*
+*/
   public ServerSetup(EmbreoParams mysettings)
   {
 
@@ -55,50 +63,71 @@ public class ServerSetup extends JPanel
     Vector PrivateServerName = new Vector();
     PrivateServerName.add(mysettings.getPrivateSoapService());
 
-    GridLayout gl = new GridLayout(3,1,10,10);
+    GridLayout gl = new GridLayout(6,1,6,6);
     
     JPanel jpWest   = new JPanel(gl);
     JPanel jpCenter = new JPanel(gl);
-    JPanel jpEast   = new JPanel(gl);
 
     setLayout(new BorderLayout());
 
-    jpWest.add(new JLabel(""));
-    jpCenter.add(new JLabel("Server"));
-    jpEast.add(new JLabel("Service Name"));
-
-// public server
+//public server
     JLabel lab = new JLabel("Public Server");
     jpWest.add(lab);
-
     publicURL = new MemoryComboBox(PublicServerURL);
     jpCenter.add(publicURL);
 
+    lab = new JLabel("Public Service Name");
+    jpWest.add(lab);
     publicName = new MemoryComboBox(PublicServerName);
-    jpEast.add(publicName);
+    jpCenter.add(publicName);
+
+//separator
+    jpWest.add(new JLabel(""));
+    jpCenter.add(new JLabel(""));
 
 //private server
     lab = new JLabel("Private Server");
     jpWest.add(lab);
     privateURL = new MemoryComboBox(PrivateServerURL);
-
     jpCenter.add(privateURL);    
 
+    lab = new JLabel("Private Service Name ");
+    jpWest.add(lab);
     privateName = new MemoryComboBox(PrivateServerName);
-    jpEast.add(privateName);
+    jpCenter.add(privateName);
+
+//separator
+    jpWest.add(new JLabel(""));
+    jpCenter.add(new JLabel(""));
  
     add(jpWest, BorderLayout.WEST);
     add(jpCenter, BorderLayout.CENTER);
-    add(jpEast, BorderLayout.EAST);
 
+//authentication  
+    userAuth = new JCheckBox("User authentication required by private server",
+                              mysettings.getUseAuth());
+    add(userAuth,BorderLayout.SOUTH);
   }
 
   public EmbreoParams setNewSettings()
   {
-    mysettings.setPublicSoapURL((String)publicURL.getSelectedItem());
-    mysettings.setPrivateSoapURL((String)privateURL.getSelectedItem());
-    mysettings.setPublicSoapService((String)publicName.getSelectedItem());
-    mysettings.setPrivateSoapService((String)privateName.getSelectedItem());
+    String settings[] = new String[5];
+    settings[0] = 
+       new String("server.public="+(String)publicURL.getSelectedItem());
+    settings[1] = 
+       new String("server.private="+(String)privateURL.getSelectedItem());
+    settings[2] = 
+       new String("service.public="+(String)publicName.getSelectedItem());
+    settings[3] = 
+       new String("service.private="+(String)privateName.getSelectedItem());
+
+    if(userAuth.isSelected())
+      settings[4] = new String("user.auth=true");
+    else
+      settings[4] = new String("user.auth=false");
+
+    mysettings.updateEmbreoPropStrings(settings);
+
     return mysettings;
   }
 
