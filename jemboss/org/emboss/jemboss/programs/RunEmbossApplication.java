0b37f7d7d35326b2b0fdcd3eaf0dcf65a9589317
timc <timc>
2002-03-25 15:33:18 +0000

    add error msg

--- jemboss/org/emboss/jemboss/programs/RunEmbossApplication.java
+++ jemboss/org/emboss/jemboss/programs/RunEmbossApplication.java
@@ -48,6 +48,8 @@ public class RunEmbossApplication
     }
     catch(IOException ioe)
     {
+      System.out.println("RunEmbossApplication Error executing: "+
+                          embossCommand);
       status = "1";
     }
   }
