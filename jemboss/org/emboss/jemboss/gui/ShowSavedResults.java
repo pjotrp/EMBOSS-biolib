e0680ec9f3e44feddc30842af8bc2a786be0876d
timc <timc>
2002-07-05 14:58:40 +0000

    add tab renderer

--- jemboss/org/emboss/jemboss/gui/ShowSavedResults.java
+++ jemboss/org/emboss/jemboss/gui/ShowSavedResults.java
@@ -198,6 +198,8 @@ public class ShowSavedResults
       listByDateRun(reslist,true);
   
       final JList st = new JList(datasets);
+      st.setCellRenderer(new TabListCellRenderer());
+
       st.addListSelectionListener(new ListSelectionListener()
       {
         public void valueChanged(ListSelectionEvent e) 
@@ -419,6 +421,7 @@ public class ShowSavedResults
       datasets.addElement(convertToPretty((String)enum.nextElement()));
 
     final JList st = new JList(datasets);
+    st.setCellRenderer(new TabListCellRenderer());
     st.addListSelectionListener(new ListSelectionListener()
     {
       public void valueChanged(ListSelectionEvent e) 
@@ -543,7 +546,7 @@ public class ShowSavedResults
 
               for(int i=0;i<sel.length;i++)
               {
-                JembossProcess jp = epr.getResult((String)sel[i]);
+                JembossProcess jp = epr.getResult(convertToOriginal(sel[i]));
                 epr.removeResult(jp);
                 datasets.removeElement(sel[i]);  // amend the list
               }
@@ -624,20 +627,102 @@ public class ShowSavedResults
   {
     int index = sorig.indexOf('_');
     if(index > -1)
-      sorig = sorig.substring(0,index) + "\t\t\t" +
+      sorig = sorig.substring(0,index) + "\t" +
               sorig.substring(index+1);
     return sorig.replace('_',' ');
   }
 
   private String convertToOriginal(Object sorig)
   {
-    String s = (String)sorig;
-    int index = s.indexOf('\t');
-    if(index > -1)
-      s = s.substring(0,index) + "_" +
-          s.substring(index+3);
-    
+    String s = ((String)sorig).replace('\t','_');
     return s.replace(' ','_');
+  }
+
+}
+
+
+/**
+*
+* Adapted from an example at
+* http://www.spindoczine.com/sbe/
+*
+*/
+class TabListCellRenderer extends JLabel implements ListCellRenderer
+{
+  protected static Border m_noFocusBorder;
+  protected FontMetrics m_fm = null;
+  protected Insets m_insets = new Insets(0, 0, 0, 0);
+
+  protected int m_defaultTab = 50;
+  protected int[] m_tabs = null;
+
+  public TabListCellRenderer()
+  {
+    super();
+    m_noFocusBorder = new EmptyBorder(1, 1, 1, 1);
+    setOpaque(true);
+    setBorder(m_noFocusBorder);
+  }
+
+  public Component getListCellRendererComponent(JList list,
+              Object value, int index, boolean isSelected, boolean cellHasFocus)     
+  {         
+    setText(value.toString());
+
+    setBackground(isSelected ? list.getSelectionBackground() : list.getBackground());
+    setForeground(isSelected ? list.getSelectionForeground() : list.getForeground());
+                
+    setFont(list.getFont());
+    setBorder((cellHasFocus) ? UIManager.getBorder("List.focusCellHighlightBorder") : m_noFocusBorder);
+
+    return this;
+  }
+
+  public void setDefaultTab(int defaultTab) { m_defaultTab = defaultTab; }
+  public int getDefaultTab() { return m_defaultTab; }
+  public void setTabs(int[] tabs) { m_tabs = tabs; }
+  public int[] getTabs() { return m_tabs; }
+  public int getTab(int index)
+  {
+    if(m_tabs == null)
+      return m_defaultTab*index;
+            
+    int len = m_tabs.length;
+    if(index >= 0 && index < len)
+      return m_tabs[index];
+
+    return m_tabs[len-1] + m_defaultTab*(index-len+1);
+  }
+
+
+  public void paint(Graphics g)
+  {
+    m_fm = g.getFontMetrics();
+      
+    g.setColor(getBackground());
+    g.fillRect(0, 0, getWidth(), getHeight());
+      getBorder().paintBorder(this, g, 0, 0, getWidth(), getHeight());
+
+    g.setColor(getForeground());
+    g.setFont(getFont());
+    m_insets = getInsets();
+    int x = m_insets.left;
+    int y = m_insets.top + m_fm.getAscent();
+
+    StringTokenizer st = new StringTokenizer(getText(), "\t");
+    while (st.hasMoreTokens()) 
+    {
+      String sNext = st.nextToken();
+      g.drawString(sNext, x, y);
+      x += m_fm.stringWidth(sNext);
+
+      if (!st.hasMoreTokens())
+        break;
+      int index = 0;
+      while (x >= getTab(index))
+      index++;
+      x = getTab(index);
+    }
   }
 
 }
