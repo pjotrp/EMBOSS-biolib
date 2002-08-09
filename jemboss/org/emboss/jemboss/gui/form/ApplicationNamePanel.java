bd71c6d9f8ed905bc8d67b577b3b7381e6ffdde9
timc <timc>
2002-08-09 13:11:38 +0000

    change to plain text

--- jemboss/org/emboss/jemboss/gui/form/ApplicationNamePanel.java
+++ jemboss/org/emboss/jemboss/gui/form/ApplicationNamePanel.java
@@ -52,42 +52,10 @@ public class ApplicationNamePanel extends JPanel
     super.paintComponent(g);
     int line_spacing = 4;
 
-//  g.setFont(new Font("Dialog", Font.BOLD, fontSize));
     Color top_color = new Color(200,10,0);
     Color side_color = new Color(200,10,0);
-
-    for (int i = 0; i < 2; i++)
-    {
-      g.setColor(top_color);
-      g.drawString(name, ShiftEast(x, i), ShiftNorth(ShiftSouth(y, i), 1));
-      g.setColor(side_color);
-      g.drawString(name, ShiftWest(ShiftEast(x, i), 1), ShiftSouth(y, i));
-    }
     g.setColor(Color.red);
-    g.drawString(name, ShiftEast(x, 1), ShiftSouth(y, 1));
-
-  }
-
-  
-
-  int ShiftNorth(int p, int distance) 
-  {
-    return (p - distance);
-  }
-
-  int ShiftSouth(int p, int distance) 
-  {
-    return (p + distance);
-  }
-
-  int ShiftEast(int p, int distance) 
-  {
-    return (p + distance);
-  }
-
-  int ShiftWest(int p, int distance) 
-  {
-    return (p - distance);
+    g.drawString(name,x,y);
   }
 
 }
