d0c2f6358acb53c15ced4de06cbbb56f57b4d163
timc <timc>
2003-06-07 09:21:49 +0000

    new docs

--- jemboss/org/emboss/jemboss/gui/Browser.java
+++ jemboss/org/emboss/jemboss/gui/Browser.java
@@ -32,23 +32,48 @@ import java.io.*;
 import java.util.Vector;
 import org.emboss.jemboss.JembossParams;
 
+/**
+*
+* Jemboss web browser
+*
+*/
 public class Browser extends JFrame implements HyperlinkListener, 
                                  ActionListener 
 {
 
+  /** URL cache combo field */
   private MemoryComboBox urlField;
+  /** HTML pane   */
   private JEditorPane htmlPane;
+  /** initial URL */
   private String initialURL;
+  /** busy cursor */
   private Cursor cbusy = new Cursor(Cursor.WAIT_CURSOR);
+  /** done cursor */
   private Cursor cdone = new Cursor(Cursor.DEFAULT_CURSOR);
 
-
+  /**
+  *
+  * @param initialURL	initial URL
+  * @param name		browser frame title
+  * @param mysettings	jemboss settings
+  *
+  */
   public Browser(String initialURL, String name, 
                  JembossParams mysettings) throws IOException
   {
     this(initialURL,name,false,"",mysettings);
   }
 
+  /**
+  *
+  * @param initialURL   initial URL
+  * @param name         browser frame title
+  * @param ltext	true if html as string past to web browser 
+  * @param text		html as string
+  * @param mysettings   jemboss settings
+  *
+  */
   public Browser(String initialURL, String name,  boolean ltext, 
                  String text, JembossParams mysettings) throws IOException
   {
@@ -97,6 +122,12 @@ public class Browser extends JFrame implements HyperlinkListener,
     setVisible(true);
   }
 
+  /**
+  *
+  * @param urlName	URL to display
+  * @param initialURL   initial URL
+  *
+  */
   public Browser(URL urlName, String initialURL) throws IOException
   {
     super(initialURL);
@@ -119,11 +150,12 @@ public class Browser extends JFrame implements HyperlinkListener,
     setVisible(true);
   }
 
-/**
-*
-* Method to create the frames menu and tool bar.
-*
-*/
+  /**
+  *
+  * Method to create the frames menu and tool bar.
+  * @param urlCache	URL cache
+  *
+  */
   private void setUpJMenuBar(Vector urlCache)
   {
     JMenuBar menuBar = new JMenuBar();
@@ -190,6 +222,11 @@ public class Browser extends JFrame implements HyperlinkListener,
 
   }
 
+  /**
+  *
+  * Set the Jemboss web browser size
+  *
+  */
   private void setBrowserSize()
   {
     Dimension screenSize = getToolkit().getScreenSize();
@@ -198,6 +235,12 @@ public class Browser extends JFrame implements HyperlinkListener,
     setBounds(width/5, height/6, width, height);
   }
 
+  /**
+  *
+  * Add the html pane to a scrollpane and set the
+  * size of the html pane
+  *
+  */
   private void addToScrollPane()
   {
     htmlPane.setEditable(false);
@@ -209,6 +252,12 @@ public class Browser extends JFrame implements HyperlinkListener,
     getContentPane().add(scrollPane, BorderLayout.CENTER);
   }
 
+  /**
+  *
+  * Override actionPerformed
+  * @param event 	action event
+  *
+  */
   public void actionPerformed(ActionEvent event) 
   {
     String url;
@@ -216,7 +265,7 @@ public class Browser extends JFrame implements HyperlinkListener,
     if (event.getSource() == urlField) 
       url = (String)urlField.getSelectedItem();
     else if (event.getActionCommand().equals("JEMBOSS"))
-      url = "http://www.hgmp.mrc.ac.uk/Software/EMBOSS/Jemboss/";
+      url = "http://www.rfcgr.mrc.ac.uk/Software/EMBOSS/Jemboss/";
     else if (event.getActionCommand().equals("BACK"))
       url = (String)urlField.getItemAt(1);
     else
@@ -235,11 +284,12 @@ public class Browser extends JFrame implements HyperlinkListener,
     setCursor(cdone);
   }
 
-/**
-*
-* Method to handle hyper link events.
-*
-*/
+  /**
+  *
+  * Method to handle hyper link events.
+  * @param event	hyper link event
+  *
+  */
   public void hyperlinkUpdate(HyperlinkEvent event) 
   {
     if (event.getEventType() == HyperlinkEvent.EventType.ACTIVATED)
@@ -261,13 +311,23 @@ public class Browser extends JFrame implements HyperlinkListener,
     }
   }
 
+  /**
+  *
+  * Display a warning message
+  * @param message	message to display
+  *
+  */
   private void warnUser(String message)
   {
     JOptionPane.showMessageDialog(this, message, "Warning", 
                                   JOptionPane.ERROR_MESSAGE);
   }
 
-
+  /**
+  *
+  * Jemboss icon button
+  *
+  */
   public class JIconButton extends JButton 
   {
     public JIconButton(ImageIcon ii) 
