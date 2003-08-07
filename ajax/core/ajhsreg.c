9f857f94615edb553302261fba90601cfc0d2638
ajb <ajb>
2003-08-07 13:55:41 +0000

    Clean up

--- ajax/core/ajhsreg.c
+++ ajax/core/ajhsreg.c
@@ -60,19 +60,25 @@ static ajlong regTotal = 0;
 ** @@
 ******************************************************************************/
 
-AjPHsRegexp ajHsRegComp (AjPStr exp) {
-  AjPHsRegexp ret;
-  ret = hsregcomp (ajStrStr(exp));
-  if (ret)
-  {
-    regAlloc += sizeof(ret);
-    regCount ++;
-    regTotal ++;
-    ajDebug("ajHsRegComp %x size %d regexp '%S'\n",
-	    ret, (int) sizeof(ret), exp);
-  }
-  return ret;
+AjPHsRegexp ajHsRegComp(AjPStr exp)
+{
+    AjPHsRegexp ret;
+
+    ret = hsregcomp(ajStrStr(exp));
+    if(ret)
+    {
+	regAlloc += sizeof(ret);
+	regCount ++;
+	regTotal ++;
+	ajDebug("ajHsRegComp %x size %d regexp '%S'\n",
+		ret, (int) sizeof(ret), exp);
+    }
+
+    return ret;
 }
+
+
+
 
 /* @func ajHsRegCompC *********************************************************
 **
@@ -83,10 +89,13 @@ AjPHsRegexp ajHsRegComp (AjPStr exp) {
 ** @@
 ******************************************************************************/
 
-AjPHsRegexp ajHsRegCompC (const char* exp) {
-
-  return hsregcomp (exp);
+AjPHsRegexp ajHsRegCompC(const char* exp)
+{
+    return hsregcomp(exp);
 }
+
+
+
 
 /* execute expression match */
 
@@ -105,16 +114,22 @@ AjPHsRegexp ajHsRegCompC (const char* exp) {
 ** @@
 ******************************************************************************/
 
-AjBool ajHsRegExec (AjPHsRegexp prog, AjPStr str) {
+AjBool ajHsRegExec(AjPHsRegexp prog, AjPStr str)
+{
 
-  if (hsregexec (prog, ajStrStr(str))) {
-    prog->orig = ajStrStr(str);
-    return ajTrue;
-  }
+    if(hsregexec(prog, ajStrStr(str)))
+    {
+	prog->orig = ajStrStr(str);
+	return ajTrue;
+    }
 
-  prog->orig = NULL;
-  return ajFalse;
+    prog->orig = NULL;
+
+    return ajFalse;
 }
+
+
+
 
 /* @func ajHsRegExecC *********************************************************
 **
@@ -131,16 +146,22 @@ AjBool ajHsRegExec (AjPHsRegexp prog, AjPStr str) {
 ** @@
 ******************************************************************************/
 
-AjBool ajHsRegExecC (AjPHsRegexp prog, const char* str) {
+AjBool ajHsRegExecC(AjPHsRegexp prog, const char* str)
+{
 
-  if (hsregexec (prog, str)) {
-    prog->orig = str ;
-    return ajTrue;
-  }
+    if(hsregexec(prog, str))
+    {
+	prog->orig = str ;
+	return ajTrue;
+    }
 
-  prog->orig = NULL;
-  return ajFalse;
+    prog->orig = NULL;
+
+    return ajFalse;
 }
+
+
+
 
 /* @func ajHsRegOffset ********************************************************
 **
@@ -156,10 +177,13 @@ AjBool ajHsRegExecC (AjPHsRegexp prog, const char* str) {
 ** @@
 ******************************************************************************/
 
-ajint ajHsRegOffset (AjPHsRegexp rp) {
-
-  return (rp->startp[0] - rp->orig);
+ajint ajHsRegOffset(AjPHsRegexp rp)
+{
+    return (rp->startp[0] - rp->orig);
 }
+
+
+
 
 /* @func ajHsRegOffsetI *******************************************************
 **
@@ -176,10 +200,13 @@ ajint ajHsRegOffset (AjPHsRegexp rp) {
 ** @@
 ******************************************************************************/
 
-ajint ajHsRegOffsetI (AjPHsRegexp rp, ajint isub) {
-
-  return (rp->startp[isub] - rp->orig);
+ajint ajHsRegOffsetI(AjPHsRegexp rp, ajint isub)
+{
+    return (rp->startp[isub] - rp->orig);
 }
+
+
+
 
 /* @func ajHsRegOffsetC *******************************************************
 **
@@ -195,10 +222,13 @@ ajint ajHsRegOffsetI (AjPHsRegexp rp, ajint isub) {
 ** @@
 ******************************************************************************/
 
-ajint ajHsRegOffsetC (AjPHsRegexp rp) {
-
-  return (rp->startp[0] - rp->orig);
+ajint ajHsRegOffsetC(AjPHsRegexp rp)
+{
+    return (rp->startp[0] - rp->orig);
 }
+
+
+
 
 /* @func ajHsRegOffsetIC ******************************************************
 **
@@ -215,10 +245,13 @@ ajint ajHsRegOffsetC (AjPHsRegexp rp) {
 ** @@
 ******************************************************************************/
 
-ajint ajHsRegOffsetIC (AjPHsRegexp rp, ajint isub) {
-
-  return (rp->startp[isub] - rp->orig);
+ajint ajHsRegOffsetIC(AjPHsRegexp rp, ajint isub)
+{
+    return (rp->startp[isub] - rp->orig);
 }
+
+
+
 
 /* @func ajHsRegLenI **********************************************************
 **
@@ -230,13 +263,17 @@ ajint ajHsRegOffsetIC (AjPHsRegexp rp, ajint isub) {
 ** @@
 ******************************************************************************/
 
-ajint ajHsRegLenI (AjPHsRegexp rp, ajint isub) {
+ajint ajHsRegLenI(AjPHsRegexp rp, ajint isub)
+{
 
-  if (!rp->startp[isub])
-    return 0;
+    if(!rp->startp[isub])
+	return 0;
 
-  return (rp->endp[isub] - rp->startp[isub]);
+    return (rp->endp[isub] - rp->startp[isub]);
 }
+
+
+
 
 /* @func ajHsRegPost **********************************************************
 **
@@ -248,15 +285,22 @@ ajint ajHsRegLenI (AjPHsRegexp rp, ajint isub) {
 ** @@
 ******************************************************************************/
 
-AjBool ajHsRegPost (AjPHsRegexp rp, AjPStr* post) {
-  if (*rp->endp[0]) {
-    (void) ajStrAssC (post, rp->endp[0]);
-    return ajTrue;
-  }
+AjBool ajHsRegPost(AjPHsRegexp rp, AjPStr* post)
+{
 
-  (void) ajStrDelReuse(post);
-  return ajFalse;
+    if(*rp->endp[0])
+    {
+	ajStrAssC(post, rp->endp[0]);
+	return ajTrue;
+    }
+
+    ajStrDelReuse(post);
+
+    return ajFalse;
 }
+
+
+
 
 /* @func ajHsRegPostC *********************************************************
 **
@@ -271,16 +315,21 @@ AjBool ajHsRegPost (AjPHsRegexp rp, AjPStr* post) {
 ** @@
 ******************************************************************************/
 
-AjBool ajHsRegPostC (AjPHsRegexp rp, const char** post) {
-  if (*rp->endp[0]) {
-    *post = rp->endp[0];
-    return ajTrue;
-  }
+AjBool ajHsRegPostC(AjPHsRegexp rp, const char** post)
+{
+    if(*rp->endp[0])
+    {
+	*post = rp->endp[0];
+	return ajTrue;
+    }
 
-  *post = 0;
+    *post = 0;
 
-  return ajFalse;
+    return ajFalse;
 }
+
+
+
 
 /* @func ajHsRegSubI **********************************************************
 **
@@ -293,26 +342,33 @@ AjBool ajHsRegPostC (AjPHsRegexp rp, const char** post) {
 ** @@
 ******************************************************************************/
 
-void ajHsRegSubI (AjPHsRegexp rp, ajint isub, AjPStr* dest) {
+void ajHsRegSubI(AjPHsRegexp rp, ajint isub, AjPStr* dest)
+{
+    ajint ilen;
 
-  ajint ilen;
+    if(!rp->startp[isub])
+    {
+	ajStrDelReuse(dest);
+	return;
+    }
 
-  if (!rp->startp[isub]) {
-    (void) ajStrDelReuse (dest);
+    if(isub >= NSUBEXP)
+    {
+	ajStrDelReuse(dest);
+	return;
+    }
+    ilen = rp->endp[isub] - rp->startp[isub];
+    ajStrModL(dest, ilen+1);
+    if(ilen)
+	strncpy((*dest)->Ptr, rp->startp[isub], ilen);
+    (*dest)->Len = ilen;
+    (*dest)->Ptr[ilen] = '\0';
+
     return;
-  }
-  if (isub >= NSUBEXP) {
-    (void) ajStrDelReuse (dest);
-    return;
-  }
-  ilen = rp->endp[isub] - rp->startp[isub];
-  (void) ajStrModL (dest, ilen+1);
-  if (ilen)
-    (void) strncpy ((*dest)->Ptr, rp->startp[isub], ilen);
-  (*dest)->Len = ilen;
-  (*dest)->Ptr[ilen] = '\0';
-  return;
 }
+
+
+
 
 /* substitute substrings */
 
@@ -328,10 +384,15 @@ void ajHsRegSubI (AjPHsRegexp rp, ajint isub, AjPStr* dest) {
 ** @@
 ******************************************************************************/
 
-void ajHsRegSub (AjPHsRegexp rp, AjPStr source, AjPStr* dest) {
-  ajHsRegSubC (rp, ajStrStr(source), dest);
-  return;
+void ajHsRegSub(AjPHsRegexp rp, AjPStr source, AjPStr* dest)
+{
+    ajHsRegSubC(rp, ajStrStr(source), dest);
+
+    return;
 }
+
+
+
 
 /* @func ajHsRegSubC **********************************************************
 **
@@ -345,82 +406,102 @@ void ajHsRegSub (AjPHsRegexp rp, AjPStr source, AjPStr* dest) {
 ** @@
 ******************************************************************************/
 
-void ajHsRegSubC (AjPHsRegexp rp, const char* source, AjPStr* dest) {
+void ajHsRegSubC(AjPHsRegexp rp, const char* source, AjPStr* dest)
+{
+    register regexp * const prog;
+    register char *src;
+    register char *dst;
+    register char c;
+    register ajint no;
+    register size_t len;
+    register size_t dstfree;
 
-  register regexp * const prog = (regexp *)rp;
-  register char *src = (char *)source;
-  register char *dst;
-  register char c;
-  register ajint no;
-  register size_t len;
-  register size_t dstfree;
+    prog = (regexp *)rp;
+    src = (char *)source;
 
-  (void) ajStrMod (dest);
-  dst = (*dest)->Ptr;
-  dstfree = (*dest)->Res - 1;
+    ajStrMod(dest);
+    dst = (*dest)->Ptr;
+    dstfree = (*dest)->Res - 1;
 
-  if (prog == NULL || source == NULL) {
-    hsregerror("NULL parameter to regsub");
-    return;
-  }
-  if ((ajuint)((unsigned char)*(prog->program)) != MAGIC) {
-    hsregerror("damaged regexp");
-    return;
-  }
-
-  while ((c = *src++) != '\0') {
-    if (c == '&')
-      no = 0;
-    else if (c == '\\' && isdigit((ajint)*src))
-      no = *src++ - '0';
-    else
-      no = -1;
-
-    if (no < 0) {	/* Ordinary character. */
-      if (c == '\\' && (*src == '\\' || *src == '&'))
-	c = *src++;
-      if (!dstfree) {
-	dstfree += (*dest)->Res;
-	(*dest)->Len = (*dest)->Res - 1;
-	(*dest)->Ptr[(*dest)->Len] = '\0';
-	(void) ajStrModL (dest, (*dest)->Res*2);
-	dst = &(*dest)->Ptr[(*dest)->Len];
-	ajStrTrace(*dest);
-      }
-      *dst++ = c;
-      dstfree--;
-    }
-    else if (prog->startp[no] != NULL && prog->endp[no] != NULL &&
-	     prog->endp[no] > prog->startp[no]) {
-      len = prog->endp[no] - prog->startp[no];
-      if (dstfree < len) {
-	(*dest)->Len = (*dest)->Res - dstfree - 1;
-	(*dest)->Ptr[(*dest)->Len] = '\0';
-	if (len > (*dest)->Res) {
-	  (void) ajStrModL (dest, (*dest)->Res+len);
-	  dstfree += len;
-	  ajStrTrace(*dest);
-	}
-	else {
-	  dstfree += (*dest)->Res;
-	  (void) ajStrModL (dest, (*dest)->Res*2);
-	  ajStrTrace(*dest);
-	}
-	dst = &(*dest)->Ptr[(*dest)->Len];
-      }
-      (void) strncpy(dst, prog->startp[no], len);
-      dst += len;
-      dstfree -= len;
-      if (*(dst-1) == '\0') {	/* strncpy hit NUL. */
-	hsregerror("damaged match string");
+    if(prog == NULL || source == NULL)
+    {
+	hsregerror("NULL parameter to regsub");
 	return;
-      }
     }
-  }
-  *dst++ = '\0';
-  (*dest)->Len = (*dest)->Res - dstfree - 1;
-  return;
+
+    if((ajuint)((unsigned char)*(prog->program)) != MAGIC)
+    {
+	hsregerror("damaged regexp");
+	return;
+    }
+
+    while((c = *src++) != '\0')
+    {
+	if(c == '&')
+	    no = 0;
+	else if(c == '\\' && isdigit((ajint)*src))
+	    no = *src++ - '0';
+	else
+	    no = -1;
+
+	if(no < 0)
+	{
+	    /* Ordinary character. */
+	    if(c == '\\' && (*src == '\\' || *src == '&'))
+		c = *src++;
+	    if(!dstfree)
+	    {
+		dstfree += (*dest)->Res;
+		(*dest)->Len = (*dest)->Res - 1;
+		(*dest)->Ptr[(*dest)->Len] = '\0';
+		ajStrModL(dest, (*dest)->Res*2);
+		dst = &(*dest)->Ptr[(*dest)->Len];
+		ajStrTrace(*dest);
+	    }
+	    *dst++ = c;
+	    dstfree--;
+	}
+	else if(prog->startp[no] != NULL && prog->endp[no] != NULL &&
+		prog->endp[no] > prog->startp[no])
+	{
+	    len = prog->endp[no] - prog->startp[no];
+	    if(dstfree < len)
+	    {
+		(*dest)->Len = (*dest)->Res - dstfree - 1;
+		(*dest)->Ptr[(*dest)->Len] = '\0';
+		if(len > (*dest)->Res)
+		{
+		    ajStrModL(dest, (*dest)->Res+len);
+		    dstfree += len;
+		    ajStrTrace(*dest);
+		}
+		else
+		{
+		    dstfree += (*dest)->Res;
+		    ajStrModL(dest, (*dest)->Res*2);
+		    ajStrTrace(*dest);
+		}
+		dst = &(*dest)->Ptr[(*dest)->Len];
+	    }
+	    strncpy(dst, prog->startp[no], len);
+	    dst += len;
+	    dstfree -= len;
+	    if(*(dst-1) == '\0')
+	    {
+		/* strncpy hit NUL. */
+		hsregerror("damaged match string");
+		return;
+	    }
+	}
+    }
+    *dst++ = '\0';
+    (*dest)->Len = (*dest)->Res - dstfree - 1;
+
+    return;
 }
+
+
+
 
 /* destructor */
 
@@ -433,17 +514,25 @@ void ajHsRegSubC (AjPHsRegexp rp, const char* source, AjPStr* dest) {
 ** @@
 ******************************************************************************/
 
-void ajHsRegFree (AjPHsRegexp* pexp) {
-  if (!pexp) return;
-  if (!*pexp) return;
-  ajDebug("ajHsRegFree %x size regexp %d\n", *pexp,
-	  (ajint) sizeof(*pexp));
-  regFreeCount += 1;
-  regFree += sizeof(**pexp);
-  regTotal --;
-  hsregfree (pexp);
-  *pexp = NULL;
+void ajHsRegFree(AjPHsRegexp* pexp)
+{
+    if(!pexp)
+	return;
+    if(!*pexp)
+	return;
+    ajDebug("ajHsRegFree %x size regexp %d\n", *pexp,
+	    (ajint) sizeof(*pexp));
+    regFreeCount += 1;
+    regFree += sizeof(**pexp);
+    regTotal --;
+    hsregfree(pexp);
+    *pexp = NULL;
+
+    return;
 }
+
+
+
 
 /* @func ajHsRegTrace *********************************************************
 **
@@ -454,34 +543,43 @@ void ajHsRegFree (AjPHsRegexp* pexp) {
 ** @@
 ******************************************************************************/
 
-void ajHsRegTrace (AjPHsRegexp exp) {
-  ajint isub;
-  ajint ilen;
-  ajint ipos;
-  static AjPStr str = NULL;
+void ajHsRegTrace(AjPHsRegexp exp)
+{
+    ajint isub;
+    ajint ilen;
+    ajint ipos;
+    static AjPStr str = NULL;
 
-  ajDebug ("  REGEXP trace\n");
-  for (isub=0; isub < NSUBEXP; isub++) {
-    if (exp->startp[isub]) {
-      ilen = exp->endp[isub] - exp->startp[isub];
-      (void) ajStrModL (&str, ilen+1);
-      (void) strncpy (str->Ptr, exp->startp[isub], ilen);
-      str->Len = ilen;
-      str->Ptr[ilen] = '\0';
-      if (!isub) {
-	ajDebug (" original string '%s'\n", exp->orig);
-	ajDebug ("    string match '%S'\n", str);
-      }
-      else {
-	ipos = exp->startp[isub] - exp->startp[0];
-	ajDebug ("    substring %2d '%S' at %d\n", isub, str, ipos);
-      }
+    ajDebug("  REGEXP trace\n");
+    for(isub=0; isub < NSUBEXP; isub++)
+    {
+	if(exp->startp[isub])
+	{
+	    ilen = exp->endp[isub] - exp->startp[isub];
+	    ajStrModL(&str, ilen+1);
+	    strncpy(str->Ptr, exp->startp[isub], ilen);
+	    str->Len = ilen;
+	    str->Ptr[ilen] = '\0';
+	    if(!isub)
+	    {
+		ajDebug(" original string '%s'\n", exp->orig);
+		ajDebug("    string match '%S'\n", str);
+	    }
+	    else
+	    {
+		ipos = exp->startp[isub] - exp->startp[0];
+		ajDebug("    substring %2d '%S' at %d\n", isub, str, ipos);
+	    }
+	}
     }
-  }
-  ajDebug ("\n");
-  (void) ajStrDelReuse (&str);
-  return;
+    ajDebug("\n");
+    ajStrDelReuse(&str);
+
+    return;
 }
+
+
+
 
 /* @func ajHsRegExit **********************************************************
 **
@@ -491,13 +589,13 @@ void ajHsRegTrace (AjPHsRegexp exp) {
 ** @@
 ******************************************************************************/
 
-void ajHsRegExit (void) {
+void ajHsRegExit(void)
+{
 
-  ajDebug ("Regexp usage (bytes): %ld allocated, %ld freed, %ld in use\n",
-	   regAlloc, regFree, regAlloc - regFree);
-  ajDebug ("Regexp usage (number): %ld allocated, %ld freed %ld in use\n",
-	   regTotal, regFreeCount, regCount);
+    ajDebug("Regexp usage (bytes): %ld allocated, %ld freed, %ld in use\n",
+	    regAlloc, regFree, regAlloc - regFree);
+    ajDebug("Regexp usage (number): %ld allocated, %ld freed %ld in use\n",
+	    regTotal, regFreeCount, regCount);
 
-  return;
+    return;
 }
-
