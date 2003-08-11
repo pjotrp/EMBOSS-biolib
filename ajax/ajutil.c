fba5631f3b4387970e99a2b2c453b677aeb5d7d0
ajb <ajb>
2003-08-11 18:44:46 +0000

    Clean up

--- ajax/ajutil.c
+++ ajax/ajutil.c
@@ -1,15 +1,39 @@
-/*
+/******************************************************************************
+** @source AJAX utility functions
 **
-** AJAX library routines
+** @author Copyright (C) 1998 Ian Longden
+** @author Copyright (C) 1998 Peter Rice
+** @version 1.0
+** @@
 **
-*/
+** This library is free software; you can redistribute it and/or
+** modify it under the terms of the GNU Library General Public
+** License as published by the Free Software Foundation; either
+** version 2 of the License, or (at your option) any later version.
+**
+** This library is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+** Library General Public License for more details.
+**
+** You should have received a copy of the GNU Library General Public
+** License along with this library; if not, write to the
+** Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+** Boston, MA  02111-1307, USA.
+******************************************************************************/
 
 #include "ajax.h"
 #include <stdarg.h>
 #include <pwd.h>
 
+
+
+
 static AjBool utilBigendian;
 static ajint utilBigendCalled = 0;
+
+
+
 
 /* @func ajExit ***************************************************************
 **
@@ -22,7 +46,7 @@ static ajint utilBigendCalled = 0;
 ** @@
 ******************************************************************************/
 
-void ajExit (void)
+void ajExit(void)
 {
     ajDebug("\nFinal Summary\n=============\n\n");
     ajLogInfo();
@@ -37,9 +61,13 @@ void ajExit (void)
     ajNamExit();
     ajMemExit();
     ajMessExit();     /* clears data for ajDebug - do this last!!!  */
-    exit (0);
+    exit(0);
+
     return;
 }
+
+
+
 
 /* @func ajExitBad ************************************************************
 **
@@ -51,11 +79,15 @@ void ajExit (void)
 ** @@
 ******************************************************************************/
 
-ajint ajExitBad (void)
+ajint ajExitBad(void)
 {
-    exit (EXIT_FAILURE);
+    exit(EXIT_FAILURE);
+
     return EXIT_FAILURE;
 }
+
+
+
 
 /* @func ajLogInfo ************************************************************
 **
@@ -65,26 +97,31 @@ ajint ajExitBad (void)
 ** @@
 ******************************************************************************/
 
-void ajLogInfo (void)
+void ajLogInfo(void)
 {
     static AjPFile logf;
-    static AjPStr logfile=NULL;
-    static AjPStr uids=NULL;
+    static AjPStr logfile = NULL;
+    static AjPStr uids    = NULL;
 
-    if (ajNamGetValueC("logfile", &logfile))
+    if(ajNamGetValueC("logfile", &logfile))
     {
 	logf = ajFileNewApp(logfile);
-	if (!logf) return;
+	if(!logf)
+	    return;
+
 	ajUtilUid(&uids),
-	ajFmtPrintF (logf, "%s\t%S\t%D\n",
-		     ajAcdProgram(),
-		     uids,
-		     ajTimeTodayF("log"));
+	ajFmtPrintF(logf, "%s\t%S\t%D\n",
+		    ajAcdProgram(),
+		    uids,
+		    ajTimeTodayF("log"));
 	ajFileClose(&logf);
     }
 
     return;
 }
+
+
+
 
 /* @func ajUtilUid ************************************************************
 **
@@ -95,33 +132,37 @@ void ajLogInfo (void)
 ** @@
 ******************************************************************************/
 
-AjBool ajUtilUid (AjPStr* dest)
+AjBool ajUtilUid(AjPStr* dest)
 {
     ajint uid;
     struct passwd* pwd;
 
-    ajDebug ("ajUtilUid\n");
+    ajDebug("ajUtilUid\n");
 
     uid = getuid();
-    if (!uid)
+    if(!uid)
     {
-	ajStrAssC (dest, "");
+	ajStrAssC(dest, "");
 	return ajFalse;
     }
 
     ajDebug("  uid: %d\n", uid);
     pwd = getpwuid(uid);
-    if (!pwd)
+    if(!pwd)
     {
-	ajStrAssC (dest, "");
+	ajStrAssC(dest, "");
 	return ajFalse;
     }
 
     ajDebug("  pwd: '%s'\n", pwd->pw_name);
 
-    ajStrAssC (dest, pwd->pw_name);
+    ajStrAssC(dest, pwd->pw_name);
+
     return ajTrue;
 }
+
+
+
 
 /* @func ajUtilBigendian ******************************************************
 **
@@ -131,7 +172,7 @@ AjBool ajUtilUid (AjPStr* dest)
 ** @@
 ******************************************************************************/
 
-AjBool ajUtilBigendian (void)
+AjBool ajUtilBigendian(void)
 {
     static union lbytes
     {
@@ -139,12 +180,13 @@ AjBool ajUtilBigendian (void)
 	ajint i;
     } data;
 
-    if (!utilBigendCalled)
+    if(!utilBigendCalled)
     {
 	utilBigendCalled = 1;
-	data.i = 0;
-	data.chars[0] = '\1';
-	if (data.i == 1)
+	data.i           = 0;
+	data.chars[0]    = '\1';
+
+	if(data.i == 1)
 	    utilBigendian = ajFalse;
 	else
 	    utilBigendian = ajTrue;
@@ -152,6 +194,9 @@ AjBool ajUtilBigendian (void)
 
     return utilBigendian;
 }
+
+
+
 
 /* @func ajUtilRev2 ***********************************************************
 **
@@ -163,7 +208,7 @@ AjBool ajUtilBigendian (void)
 ** @@
 ******************************************************************************/
 
-void ajUtilRev2 (short* sval)
+void ajUtilRev2(short* sval)
 {
     union lbytes
     {
@@ -175,9 +220,10 @@ void ajUtilRev2 (short* sval)
     ajint i;
 
     data.s = *sval;
-    cs = data.chars;
-    cd =&revdata.chars[1];
-    for (i=0; i < 2; i++)
+    cs     = data.chars;
+    cd     = &revdata.chars[1];
+
+    for(i=0; i < 2; i++)
     {
 	*cd = *cs++;
 	--cd;
@@ -187,6 +233,9 @@ void ajUtilRev2 (short* sval)
 
     return;
 }
+
+
+
 
 /* @func ajUtilRev4 ***********************************************************
 **
@@ -198,7 +247,7 @@ void ajUtilRev2 (short* sval)
 ** @@
 ******************************************************************************/
 
-void ajUtilRev4 (ajint* ival)
+void ajUtilRev4(ajint* ival)
 {
     union lbytes
     {
@@ -210,9 +259,9 @@ void ajUtilRev4 (ajint* ival)
     ajint i;
 
     data.i = *ival;
-    cs = data.chars;
-    cd =&revdata.chars[3];
-    for (i=0; i < 4; i++)
+    cs     = data.chars;
+    cd     = &revdata.chars[3];
+    for(i=0; i < 4; i++)
     {
 	*cd = *cs++;
 	--cd;
@@ -222,6 +271,9 @@ void ajUtilRev4 (ajint* ival)
 
     return;
 }
+
+
+
 
 /* @func ajUtilRev8 ***********************************************************
 **
@@ -233,7 +285,7 @@ void ajUtilRev4 (ajint* ival)
 ** @@
 ******************************************************************************/
 
-void ajUtilRev8 (ajlong* ival)
+void ajUtilRev8(ajlong* ival)
 {
     union lbytes
     {
@@ -245,9 +297,9 @@ void ajUtilRev8 (ajlong* ival)
     ajint i;
     
     data.l = *ival;
-    cs = data.chars;
-    cd =&revdata.chars[7];
-    for (i=0; i < 8; i++)
+    cs     = data.chars;
+    cd     = &revdata.chars[7];
+    for(i=0; i < 8; i++)
     {
 	*cd = *cs++;
 	--cd;
@@ -257,6 +309,9 @@ void ajUtilRev8 (ajlong* ival)
     
     return;
 }
+
+
+
 
 /* @func ajUtilCatch **********************************************************
 **
@@ -270,9 +325,9 @@ void ajUtilRev8 (ajlong* ival)
 ** @@
 ******************************************************************************/
 
-void ajUtilCatch (void)
+void ajUtilCatch(void)
 {
-    static ajint calls=0;
+    static ajint calls = 0;
 
     calls = calls + 1;
 
