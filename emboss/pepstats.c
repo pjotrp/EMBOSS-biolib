353e8837d69d38783a0997bf9e3dcf99b8b7b60f
ajb <ajb>
2003-09-23 13:52:56 +0000

    Changed to allow multiple sequences

--- emboss/pepstats.c
+++ emboss/pepstats.c
@@ -52,7 +52,7 @@ int main(int argc, char **argv)
 	"Basic\t\t(H+K+R)\t\t\t","Acidic\t\t(B+D+E+Z)\t\t"
     };
 
-    AjPSeq a;
+    AjPSeqall a;
     AjPStr substr;
     ajint be;
     ajint en;
@@ -77,19 +77,16 @@ int main(int argc, char **argv)
     float *dhstat = NULL;
     AjPStr datafn = NULL;
     AjPFile mfptr = NULL;
-
+    AjPSeq  seq = NULL;
+    
     embInit("pepstats", argc, argv);
 
-    a       = ajAcdGetSeq("sequence");
+    a       = ajAcdGetSeqall("sequence");
     termini = ajAcdGetBool("termini");
     outf    = ajAcdGetOutfile("outfile");
     datafn  = ajAcdGetString("aadata");
 
     substr = ajStrNew();
-    be = ajSeqBegin(a);
-    en = ajSeqEnd(a);
-    ajStrAssSubC(&substr,ajSeqChar(a),be-1,en-1);
-    len = en-be+1;
 
 
     ajFileDataNew(datafn, &mfptr);
@@ -102,78 +99,89 @@ int main(int argc, char **argv)
     if(!embReadAminoDataFloatC(DAYHOFF_FILE,&dhstat,0.001))
 	ajFatal("Set the EMBOSS_DATA environment variable");
 
-
     AJCNEW (c, EMBIEPSIZE);
 
-    embIepComp(ajStrStr(substr),1,c);
-    if(!termini)
-	c[EMBIEPAMINO]=c[EMBIEPCARBOXYL]=0;
-
-    ajFmtPrintF(outf,"PEPSTATS of %s from %d to %d\n\n",ajSeqName(a),
-		be,en);
-
-    ajFmtPrintF(outf,"Molecular weight = %-10.2f\t\tResidues = %-6d\n",
-		(molwt=embPropCalcMolwt(ajStrStr(substr),0,len-1)),len);
-
-
-    for(i=0,charge=0.0;i<26;++i)
-	charge += (double)c[i] * EmbPropTable[i][EMBPROPCHARGE];
-
-    ajFmtPrintF(outf,"Average Residue Weight  = %-7.3f \tCharge   = %-6.1f\n",
-		molwt/(double)len,charge);
-
-    if(!embIepIEP(ajStrStr(substr),1,&iep,termini))
-	ajFmtPrintF(outf,"Isoelectric Point = None\n\n");
-    else
-	ajFmtPrintF(outf,"Isoelectric Point = %-6.4lf\n",iep);
-
-    ngps = c['N'-'A'] + c['G'-'A'] + c['P'-'A'] + c['S'-'A'];
-    rk   = c['R'-'A'] + c['K'-'A'];
-    de   = c['D'-'A'] + c['E'-'A'];
-
-    cv = (double)LAM1*((double)((double)ngps/(double)len)) +
-	 (double)LAM2*fabs((double)(((double)(rk-de)/(double)len)-
-				    (double)0.03));
-
-    psolu = 0.4934 + 0.276*fabs((double)(cv-CVDISC)) -
-	0.0392*(cv-CVDISC)*(cv-CVDISC);
-
-    if(cv-CVDISC >= 0.0)
-	ajFmtPrintF(outf,"Imp");
-    else
-	ajFmtPrintF(outf,"P");
-    ajFmtPrintF(outf,
-		"robability of expression in inclusion bodies = %.3lf\n\n",
-		psolu);
-
-
-    ajFmtPrintF(outf,"Residue\t\tNumber\t\tMole%%\t\tDayhoffStat\n");
-    for(i=0;i<26;++i)
+    while(ajSeqallNext(a,&seq))
     {
-	if(!EmbPropTable[i][EMBPROPMOLWT])
-	    continue;
+	be = ajSeqallBegin(a);
+	en = ajSeqallEnd(a);
+	ajStrAssSubC(&substr,ajSeqChar(seq),be-1,en-1);
+	len = en-be+1;
 
-	molpc=(100.0 * (double)c[i]/(double)len);
-	ajFmtPrintF(outf,"%c = %s\t\t%d\t\t%-7.3f\t\t%-7.3f\t\n",i+'A',
-		    embPropIntToThree(i),c[i],molpc,molpc/dhstat[i]);
+
+
+	embIepComp(ajStrStr(substr),1,c);
+	if(!termini)
+	    c[EMBIEPAMINO]=c[EMBIEPCARBOXYL]=0;
+
+	ajFmtPrintF(outf,"PEPSTATS of %s from %d to %d\n\n",ajSeqName(seq),
+		    be,en);
+
+	ajFmtPrintF(outf,"Molecular weight = %-10.2f\t\tResidues = %-6d\n",
+		    (molwt=embPropCalcMolwt(ajStrStr(substr),0,len-1)),len);
+
+
+	for(i=0,charge=0.0;i<26;++i)
+	    charge += (double)c[i] * EmbPropTable[i][EMBPROPCHARGE];
+
+	ajFmtPrintF(outf,"Average Residue Weight  = %-7.3f \t"
+		    "Charge   = %-6.1f\n", molwt/(double)len,charge);
+
+	if(!embIepIEP(ajStrStr(substr),1,&iep,termini))
+	    ajFmtPrintF(outf,"Isoelectric Point = None\n\n");
+	else
+	    ajFmtPrintF(outf,"Isoelectric Point = %-6.4lf\n",iep);
+
+	ngps = c['N'-'A'] + c['G'-'A'] + c['P'-'A'] + c['S'-'A'];
+	rk   = c['R'-'A'] + c['K'-'A'];
+	de   = c['D'-'A'] + c['E'-'A'];
+
+	cv = (double)LAM1*((double)((double)ngps/(double)len)) +
+	    (double)LAM2*fabs((double)(((double)(rk-de)/(double)len)-
+				       (double)0.03));
+
+	psolu = 0.4934 + 0.276*fabs((double)(cv-CVDISC)) -
+	    0.0392*(cv-CVDISC)*(cv-CVDISC);
+
+	if(cv-CVDISC >= 0.0)
+	    ajFmtPrintF(outf,"Imp");
+	else
+	    ajFmtPrintF(outf,"P");
+	ajFmtPrintF(outf,
+		    "robability of expression in inclusion bodies = %.3lf\n\n",
+		    psolu);
+
+
+	ajFmtPrintF(outf,"Residue\t\tNumber\t\tMole%%\t\tDayhoffStat\n");
+	for(i=0;i<26;++i)
+	{
+	    if(!EmbPropTable[i][EMBPROPMOLWT])
+		continue;
+
+	    molpc=(100.0 * (double)c[i]/(double)len);
+	    ajFmtPrintF(outf,"%c = %s\t\t%d\t\t%-7.3f\t\t%-7.3f\t\n",i+'A',
+			embPropIntToThree(i),c[i],molpc,molpc/dhstat[i]);
+	}
+
+
+	ajFmtPrintF(outf,"\nProperty\tResidues\t\tNumber\t\tMole%%\n");
+	for(i=1;i<10;++i)
+	{
+	    ajFmtPrintF(outf,"%s",prop[i-1]);
+	    for(j=0,sum=0;j<26;++j)
+		if(EmbPropTable[j][i])
+		    sum += c[j];
+	    ajFmtPrintF(outf,"%d\t\t%6.3f\n",sum,
+			100.0 * (double)sum/(double)len);
+	}
+	ajFmtPrintF(outf,"\n");
     }
-
-
-    ajFmtPrintF(outf,"\nProperty\tResidues\t\tNumber\t\tMole%%\n");
-    for(i=1;i<10;++i)
-    {
-	ajFmtPrintF(outf,"%s",prop[i-1]);
-	for(j=0,sum=0;j<26;++j)
-	    if(EmbPropTable[j][i])
-		sum += c[j];
-	ajFmtPrintF(outf,"%d\t\t%6.3f\n",sum,100.0 * (double)sum/(double)len);
-    }
+    
 
 
     AJFREE(dhstat);
     AJFREE(c);
 
-    ajSeqDel(&a);
     ajStrDel(&substr);
     ajFileClose(&outf);
     ajFileClose(&mfptr);
