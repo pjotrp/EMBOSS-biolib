2e8bef17b6b8eaca1b45ca98dad1aab2c637787a
counsell <counsell>
2004-03-09 14:51:04 +0000

    fixed (obvious) memory leaks

--- emboss/contactcount.c
+++ emboss/contactcount.c
@@ -30,12 +30,17 @@
 /* DDDDEBUG */
 #define DEBUG_LEVEL 0
 
-/* [XXXX ajIntCount1, ajIntCount2 SHOULD BE REPLACED WITH MORE MEANINGFUL NAMES] */
 static AjPFeature write_count(AjPFeattable ajpFeattableCounts,
 			      ajint ajIntFeatureResType,
-			      ajint ajIntCount1,
-			      ajint ajIntCount2);
+			      ajint ajIntSpecificPairs,
+			      ajint ajIntTotalPairs);
 
+static AjBool write_scoring_matrix(AjPFile ajpFileScoringMatrix,
+				   AjPStr ajpStrOriginalAlphabet);
+
+static ajint  char_to_BLOSUM_index(char cResType);
+
+static ajint BLOSUM_index_to_char(ajint ajIntBlosumIndex);
 
 
 
@@ -47,65 +52,64 @@ static AjPFeature write_count(AjPFeattable ajpFeattableCounts,
 
 int main( int argc , char **argv )
 {
-    /* DDDDEBUG: string for test prints */
-    AjPStr ajpStrDebug = NULL;
-
-    AjPStr ajpStrCmapFileName   = NULL;
-    AjPList ajpListCmapFiles    = NULL; /* list of contact map files */
 
     /* position counters and limits */
     ajint ajIntResTypeCurrent = 0;
-    ajint ajIntContactNumber      = 0;
+    ajint ajIntContactNumber  = 0;
     ajint ajIntNumberOfContactFiles = 0;
-    ajint ajIntRow = 0;
+    ajint ajIntRow    = 0;
     ajint ajIntColumn = 0;
-    ajint ajIntRowMax = 0;
+    ajint ajIntRowMax    = 0;
     ajint ajIntColumnMax = 0;
 
-    /* contact map */
-    AjPFile ajpFileCmapCurrent   = NULL; /* file                      */
-    AjPCmap ajpCmapCurrent       = NULL; /* object                    */
-    AjPInt2d ajpInt2dContactMap  = NULL; /* Cmap matrix               */
-    AjPStr ajpStrChainId         = NULL; /* chain id attribute        */
-    AjPStr ajpStrChainSeq        = NULL; /* seq string attribute      */
-    AjPSeq ajpSeqChain           = NULL; /* seq AjPStr as seq object  */
-    char cFirstResType       = '\0'; /* first residue in contact  */
-    char cSecondResType      = '\0'; /* second residue in contact */
-    ajint ajIntFirstResType  = 0;    /* first residue in contact  */
-    ajint ajIntSecondResType = 0;    /* second residue in contact */
+    /* contact maps */
+    AjPStr ajpStrCmapFileName   = NULL; /* name                      */
+    AjPFile ajpFileCmapCurrent  = NULL; /* file                      */
+    AjPList ajpListCmapFiles    = NULL; /* list of contact map files */
+    AjPCmap ajpCmapCurrent      = NULL; /* object                    */
+    AjPInt2d ajpInt2dContactMap = NULL; /* Cmap matrix               */
+    AjPStr ajpStrChainId        = NULL; /* chain id attribute        */
+    AjPStr ajpStrChainSeq       = NULL; /* seq string attribute      */
+    AjPSeq ajpSeqChain          = NULL; /* seq AjPStr as seq object  */
+
+    char cFirstResType       = '\0'; /* first residue letter in contact  */
+    char cSecondResType      = '\0'; /* second residue letter in contact */
+    ajint ajIntFirstResType  = 0;    /* first residue number in contact  */
+    ajint ajIntSecondResType = 0;    /* second residue number in contact */
 
     /* count matrix */
-    AjPStr ajStrNaturalAlphabet;
-    AjPStr ajStrResTypeAlphabet;
-    AjPStr ajStrFirstResType;
-    AjPStr ajStrSecondResType;
-    AjIStr ajStrIterFirstResType;
-    AjIStr ajStrIterSecondResType;
-    
+    AjPStr ajpStrNaturalAlphabet   = NULL; /* ordered letters of Eng. lang */
+    AjPStr ajpStrResTypeAlphabet   = NULL; /* ordered letters of residues  */
+    AjPStr ajpStrFirstResType      = NULL;
+    AjPStr ajpStrSecondResType     = NULL;
+    AjIStr ajpStrIterFirstResType  = NULL;
+    AjIStr ajpStrIterSecondResType = NULL;
 
     /* scoring matrix */
-    ajint ajIntNumberOfResTypes = 0;
-    AjPInt2d ajpInt2dCounts         = NULL;
-    ajint ajIntTempCount            = 0;
+    ajint ajIntNumberOfResTypes;
+    AjPInt2d ajpInt2dCounts = NULL;
+    ajint ajIntTempCount = 0;
+    AjPFile ajpFile2dScoringMatrix = NULL;
 
     /* number of contacts between first and second residue types */
-    ajint ajIntCount1 = 0;
+    ajint ajIntSpecificPairs = 0;
     /* total number of contacts of first residue type */
-    ajint ajIntCount2 = 0;
+    ajint ajIntTotalPairs = 0;
 
-    /* output report file for counts */
-    AjPReport ajpReportCounts = NULL;
-    AjPStr ajpStrReportHeader = NULL;
-
-    /* DDDDEBUG: string for report footer */
-    AjPStr ajpStrReportTail          = NULL;
-    AjPFeattable ajpFeattableCounts  = NULL;
-    AjPFeature ajpFeatCurrent        = NULL;
+    /* report objects */
+    AjPReport ajpReportCounts       = NULL;
+    AjPStr ajpStrReportHead         = NULL;
+    AjPFeattable ajpFeattableCounts = NULL;
+    AjPFeature ajpFeatCurrent       = NULL;
+    AjPStr ajpStrReportTail         = NULL; /* DDDDEBUGGING ONLY */
 
     embInit( "contactcount", argc, argv );
 
     /* get contact file directory from ACD */
     ajpListCmapFiles = ajAcdGetDirlist("cmapdir");
+
+    /* get test output file from ACD */
+    ajpFile2dScoringMatrix = ajAcdGetOutfile("dummyfile");
 
     /* get contact count output file from ACD */
     ajpReportCounts = ajAcdGetReport("outfile");
@@ -113,8 +117,10 @@ int main( int argc , char **argv )
     ajIntNumberOfContactFiles = ajListLength(ajpListCmapFiles);
 
     /* create a 2-D array (count array) to store the scores */
-    ajStrNaturalAlphabet = ajStrNewC("ABCDEFGHIJKLMNOPQRSTUVWZYZ");
-    ajIntNumberOfResTypes = ajStrLen(ajStrNaturalAlphabet);
+    ajpStrNaturalAlphabet = ajStrNewC("ABCDEFGHIJKLMNOPQRSTUVWZYZ");
+    ajpStrResTypeAlphabet = ajStrNewC("ARNDCQEGHILKMFPSTWYVBZX*");
+
+    ajIntNumberOfResTypes = ajStrLen(ajpStrNaturalAlphabet);
     ajpInt2dCounts = ajInt2dNewL(ajIntNumberOfResTypes);
 
     /* empty count array */
@@ -126,7 +132,12 @@ int main( int argc , char **argv )
 	}
     }
 
-    /* BEGIN LOOP OVER CONTACT MAP FILES IN CURRENT DIRECTORY */
+    /* reserve memory for objects in loop */
+    ajpSeqChain = ajSeqNewStr(ajpStrChainSeq);
+    ajpStrReportHead = ajStrNew();
+    ajpFeattableCounts = ajFeattableNewSeq(ajpSeqChain);
+
+    /* loop over contact map files in current directory */
     while(ajListPop(ajpListCmapFiles, (void **)&ajpStrCmapFileName))
     {
 	ajpFileCmapCurrent = ajFileNewIn(ajpStrCmapFileName);
@@ -149,7 +160,6 @@ int main( int argc , char **argv )
 
 	/* obtain sequence object from string */
 	ajpStrChainSeq = ajpCmapCurrent->Seq;
-	ajpSeqChain = ajSeqNewStr(ajpStrChainSeq);
 
 	/* obtain contact matrix from Cmap object */
 	ajpInt2dContactMap = ajpCmapCurrent->Mat;
@@ -158,15 +168,27 @@ int main( int argc , char **argv )
 	ajInt2dLen(ajpInt2dContactMap, &ajIntRowMax, &ajIntColumnMax);
 
 	/* loop through all cells in map */
-	for(ajIntRow = 0;ajIntRow<ajIntRowMax;ajIntRow++)
+	for(ajIntRow = 0;ajIntRow < ajIntRowMax;ajIntRow++)
 	{
 	    cFirstResType = ajStrChar(ajpStrChainSeq, ajIntRow);
 	    for(ajIntColumn = 0;ajIntColumn < ajIntColumnMax;ajIntColumn++)
 	    {
 		cSecondResType = ajStrChar(ajpStrChainSeq, ajIntColumn);
-		ajIntFirstResType = ajAZToInt(cFirstResType);
-		ajIntSecondResType = ajAZToInt(cSecondResType);
-		if(ajInt2dGet(ajpInt2dContactMap, ajIntRow, ajIntColumn))
+/* 		ajIntFirstResType = ajAZToInt(cFirstResType); */
+/* 		ajIntSecondResType = ajAZToInt(cSecondResType); */
+		ajIntFirstResType = char_to_BLOSUM_index(cFirstResType);
+		ajIntSecondResType = char_to_BLOSUM_index(cSecondResType);
+		if((ajIntFirstResType < 0) || (ajIntFirstResType > 27))
+		{
+		    ajFmtPrint("\n ======================\n");
+		    ajFmtPrint("\n OUT OF RANGE. FIRST RESIDUE = %d", ajIntFirstResType);
+		}
+		else if((ajIntSecondResType < 0) || (ajIntSecondResType > 27))
+		{
+		    ajFmtPrint("\n ======================\n");
+		    ajFmtPrint("\n OUT OF RANGE. SECOND RESIDUE = %d", ajIntSecondResType);
+		}
+		else if(ajInt2dGet(ajpInt2dContactMap, ajIntRow, ajIntColumn))
 		{
 		    ajIntTempCount = ajInt2dGet(ajpInt2dCounts,
 						ajIntFirstResType,
@@ -191,24 +213,21 @@ int main( int argc , char **argv )
 	/* get dimensions of count array */
 	ajInt2dLen(ajpInt2dCounts, &ajIntRowMax, &ajIntColumnMax);
 
-	/* chain info for head of report */
-	ajFmtPrintS(&ajpStrReportHeader, "Chain: %S", (ajpStrChainId));
-	ajReportSetHeader(ajpReportCounts, ajpStrReportHeader);
-
-	/* create feature table for count output */
-	ajpFeattableCounts = ajFeattableNewSeq(ajpSeqChain);
+	/* info about chain for head of report */
+	ajFmtPrintS(&ajpStrReportHead, "Chain: %S", (ajpStrChainId));
+	ajReportSetHeader(ajpReportCounts, ajpStrReportHead);
 
 	/* DDDDEBUG: DUMMY VALUE FOR CONTACT NUMBER */
 	ajIntContactNumber= ajSeqLen(ajpSeqChain)-5;
 
 	/* DDDDEBUGGING */
-	ajIntCount1 = 4;
-	ajIntCount2 = 7;
+	ajIntSpecificPairs = 4;
+	ajIntTotalPairs = 7;
 
 	ajpFeatCurrent = write_count(ajpFeattableCounts,
 				     ajIntContactNumber,
-				     ajIntCount1,
-				     ajIntCount2);
+				     ajIntSpecificPairs,
+				     ajIntTotalPairs);
 
 	/* write the report to the output file */
 	ajReportWrite(ajpReportCounts,
@@ -234,71 +253,54 @@ int main( int argc , char **argv )
     {
 	for(ajIntColumn = 0;ajIntColumn < ajIntNumberOfResTypes;ajIntColumn++)
 	{
-	    cFirstResType = ajIntToAZ(ajIntRow);
-	    cSecondResType = ajIntToAZ(ajIntColumn);
+/* 	    cFirstResType = ajIntToAZ(ajIntRow); */
+/* 	    cSecondResType = ajIntToAZ(ajIntColumn); */
+	    cFirstResType = BLOSUM_index_to_char(ajIntRow);
+	    cSecondResType = BLOSUM_index_to_char(ajIntColumn);
 	    ajIntTempCount = ajInt2dGet(ajpInt2dCounts, ajIntRow, ajIntColumn);
 	    ajFmtPrint("%d\t", ajIntTempCount);
 	}
 	ajFmtPrint("\n");
     }
 
-    /* new string iterator over BLOSUM list of residue types */
-    ajStrResTypeAlphabet = ajStrNewC("ARNDCQEGHILKMFPSTWYVBZX*");
-    ajStrIterFirstResType = ajStrIter(ajStrResTypeAlphabet);
 
-    ajIntRow = 0;
-    ajIntColumn =0;
+    /* XXXX WHY DOESN'T THIS RETURN A VALID POINTER? */
+/*     ajpFile2dScoringMatrix = ajFileNewOutC("/users/damian/EMBOSS/emboss/emboss/emboss/contacttest/dummyfile.dat"); */
     
-    /* write scores to data file */
-    while(ajStrIterNext(ajStrIterFirstResType))
-    {
-	cFirstResType = ajStrIterGetK(ajStrIterFirstResType);
-	ajIntRow = ajAZToInt(cFirstResType);
-	ajStrIterSecondResType = ajStrIter(ajStrResTypeAlphabet);
-	while(ajStrIterNext(ajStrIterSecondResType))
-	{
-	    cSecondResType = ajStrIterGetK(ajStrIterSecondResType);
-	    ajIntColumn = ajAZToInt(cSecondResType);
-	    /* DDDDEBUG */
-	    ajFmtPrint("(%4c, %4c)-", cFirstResType, cSecondResType);
-	    ajFmtPrint("(%4d, %4d) ", ajIntRow, ajIntColumn);
-	}
-	ajFmtPrint("\n");
-	ajStrIterFree(&ajStrIterSecondResType);
-    }
+    /* write scores to scoring matrix data file */
+    write_scoring_matrix(ajpFile2dScoringMatrix,
+			 ajpStrNaturalAlphabet);
 
     /* DDDDEBUG TEST INFO FOR TAIL OF REPORT */
     /*     ajFmtPrintS(&ajpStrReportTail, "This is some tail text"); */
     /*     ajReportSetTail(ajpReportCounts, ajpStrReportTail); */
 
-
-    /* close the report file */
+    /* clear up report objects */
+    ajStrDel(&ajpStrReportHead);
+    ajFeatDel(&ajpFeatCurrent);
+    ajFeattableDel(&ajpFeattableCounts);
     ajReportDel(&ajpReportCounts);
 
-    /* clear up report objects */
-    ajFeattableDel(&ajpFeattableCounts);
-    
-    /* clean up the list iterator */
-
-    /* clean up the list of files */
+    /*
+     * clear up the contact map objects,
+     * starting with the file objects
+     */
     while(ajListPop(ajpListCmapFiles, (void **)&ajpFileCmapCurrent))
 	AJFREE(ajpFileCmapCurrent);
     ajListFree(&ajpListCmapFiles);
-    
-    /* clear up the contact map object */
-    ajXyzCmapDel(&ajpCmapCurrent);
-    
-    /* clean up the derived objects */
-    ajStrDel(&ajpStrChainId);
     ajSeqDel(&ajpSeqChain);
-    ajStrDel(&ajpStrChainSeq);
+    ajInt2dDel(&ajpInt2dCounts);
 
-    /* tidy up other stuff */
-    ajStrIterFree(&ajStrIterFirstResType);
-    ajStrIterFree(&ajStrIterSecondResType);
-    ajStrDel(&ajStrFirstResType);
-    ajStrDel(&ajStrSecondResType);
+    /* tidy up scoring output objects */
+    ajStrIterFree(&ajpStrIterFirstResType);
+    ajStrIterFree(&ajpStrIterSecondResType);
+    ajStrDel(&ajpStrFirstResType);
+    ajStrDel(&ajpStrSecondResType);
+    ajStrDel(&ajpStrNaturalAlphabet);
+    ajStrDel(&ajpStrResTypeAlphabet);    
     
+    ajFileOutClose(&ajpFile2dScoringMatrix);
+
     /*  tidy up everything else... */
     ajExit();
 
@@ -313,21 +315,21 @@ int main( int argc , char **argv )
 ** writes frequency features to a feature table and returns new feature  
 **
 ** @param [r] ajpFeattableCounts [AjPFeattable] table to write frequency to
-** @param [r] ajIntFeatureResType [ajint] residue type that count
+** @param [r] ajIntFeatureResType [ajint] residue type that a count
 **                                            belongs to
-** @param [r] ajIntCount1 [ajint] phi torsion angle for residue
-** @param [r] ajIntCount2 [ajint] psi torsion angle for residue
+** @param [r] ajIntSpecificPairs [ajint] contacts specific to both residues
+** @param [r] ajIntTotalPairs [ajint] contacts specific to one residue
 ** @return [AjPFeature] New feature added to feature table
 ** @@
 ******************************************************************************/
 
-static AjPFeature write_count (AjPFeattable ajpFeattableCounts,
-			       ajint ajIntFeatureResType,
-			       ajint ajIntCount1,
-			       ajint ajIntCount2)
+static AjPFeature write_count(AjPFeattable ajpFeattableCounts,
+			      ajint ajIntFeatureResType,
+			      ajint ajIntSpecificPairs,
+			      ajint ajIntTotalPairs)
 {
-    AjPFeature ajpFeatCounts;
-    AjPStr ajpStrFeatTemp;
+    AjPFeature ajpFeatCounts = NULL;
+    AjPStr ajpStrFeatTemp = NULL;
 
     ajpStrFeatTemp = ajStrNew();
 
@@ -335,9 +337,9 @@ static AjPFeature write_count (AjPFeattable ajpFeattableCounts,
     ajpFeatCounts = ajFeatNewII(ajpFeattableCounts,
 				ajIntFeatureResType,
 				ajIntFeatureResType);
-    ajFmtPrintS(&ajpStrFeatTemp, "*count1 %d", ajIntCount1);
+    ajFmtPrintS(&ajpStrFeatTemp, "*count1 %d", ajIntSpecificPairs);
     ajFeatTagAdd(ajpFeatCounts, NULL, ajpStrFeatTemp);
-    ajFmtPrintS(&ajpStrFeatTemp, "*count2 %d", ajIntCount2);
+    ajFmtPrintS(&ajpStrFeatTemp, "*count2 %d", ajIntTotalPairs);
     ajFeatTagAdd(ajpFeatCounts, NULL, ajpStrFeatTemp);
 
     ajStrDel(&ajpStrFeatTemp);
@@ -347,4 +349,140 @@ static AjPFeature write_count (AjPFeattable ajpFeattableCounts,
 
 
 
+/* @funcstatic write_scoring_matrix ******************************************
+**
+** writes normalized substitution scores to a scoring matrix file format
+**
+** @param [r] ajpFileScoringMatrix [AjPFile] file to write scoring table to
+** @param [r] ajpStrOriginalAlphabet [AjPStr] chars of input residue types in order
+** @param [r] ajpInt2dScoringMatrix [AjPInt2d] 2-D matrix of signed integer scores
+** @return [AjBool] AJTRUE for success
+** @@
+******************************************************************************/
 
+static AjBool write_scoring_matrix(AjPFile ajpFileScoringMatrix,
+				   AjPStr ajpStrOriginalAlphabet)
+{
+    ajint ajIntRow      = 0;
+    ajint ajIntColumn   = 0;
+    char cRowResType    = '\0';
+    char cColumnResType = '\0';
+    AjBool ajBoolSuccess = AJFALSE;
+    AjPStr ajpStrResTypeAlphabet     = NULL; /* alphabet strings */
+    AjPStr ajpStrResTypeAlphabetCopy = NULL;
+    AjIStr ajpStrIterRowResType      = NULL;
+    AjIStr ajpStrIterColumnResType   = NULL;
+
+    ajIntRow = 0;
+    ajIntColumn =0;
+
+    /* write scores to data file */
+    ajpStrResTypeAlphabet = ajStrNewC("ARNDCQEGHILKMFPSTWYVBZX*");
+    ajpStrResTypeAlphabetCopy = ajStrDup(ajpStrResTypeAlphabet);
+    ajpStrIterRowResType  = ajStrIter(ajpStrResTypeAlphabet);
+    
+    /* iterate over string of BLOSUM residue types */
+    do
+    {
+	cRowResType = ajStrIterGetK(ajpStrIterRowResType);
+/* 	ajIntRow = ajAZToInt(cRowResType); */
+	ajIntRow = char_to_BLOSUM_index(cRowResType);
+	ajpStrIterColumnResType = ajStrIter(ajpStrResTypeAlphabetCopy);
+
+	/* iterate over string of BLOSUM residue types */
+	do
+	{
+	    cColumnResType = ajStrIterGetK(ajpStrIterColumnResType);
+	    ajIntColumn = char_to_BLOSUM_index(cColumnResType);
+	    /* DDDDEBUG */
+	    ajFmtPrint("(%4c, %4c)-", cRowResType, cColumnResType);
+	    ajFmtPrint("(%4d, %4d) ", ajIntRow, ajIntColumn);
+	    ajFmtPrintF(ajpFileScoringMatrix, "(%4c, %4c)-", cRowResType, cColumnResType);
+	    ajFmtPrintF(ajpFileScoringMatrix, "(%4d, %4d) ", ajIntRow, ajIntColumn);
+	}
+	while(ajStrIterNext(ajpStrIterColumnResType));
+
+	ajFmtPrint("\n");
+	/* KKKK kludge to rewind string iterator */
+	ajStrIterFree(&ajpStrIterColumnResType);
+    }    
+    while(ajStrIterNext(ajpStrIterRowResType));
+
+    ajStrDel(&ajpStrResTypeAlphabet);
+    ajStrIterFree(&ajpStrIterRowResType);
+  
+    return ajBoolSuccess;
+}
+
+
+
+
+/* @funcstatic char_to_BLOSUM_index ******************************************
+**
+** converts residue type character to BLOSUM index
+**
+** @param [r] cResType [char] original single-letter residue type
+** @return [ajint] index to corresponding character in data dir scoring matrix
+** @@
+******************************************************************************/
+
+static ajint char_to_BLOSUM_index(char cResType)
+{
+    ajint ajIntBlosumIndex;
+    static const ajint ajIntAsciiOffset = -65;
+    static const ajint ajIntAsterisk = 91;
+    static const ajint ajIntQuestionMark = 92;
+
+    /* look up array for residue type order
+     *  found in EBLOSUM data files:
+     *   ARNDCQEGHILKMFPSTWYVBZX*
+     */
+    const ajint ajIntArrayLookUp[28] =
+	{0,20,4,3,6,13,7,8,9,24,11,10,12,2,25,14,5,1,15,16,26,19,17,22,18,21,23,27};
+    if (cResType == '*')
+	cResType = ajIntAsterisk;
+    if (cResType == '?')
+	cResType = ajIntQuestionMark;
+  
+    cResType = cResType + ajIntAsciiOffset;
+    
+    /* default to out-of-range value */
+    if ((cResType < 0) || (cResType > 27))
+	ajIntBlosumIndex = 28;
+    else
+	ajIntBlosumIndex = ajIntArrayLookUp[(ajint) cResType];
+
+    return ajIntBlosumIndex;
+}
+
+
+
+/* @funcstatic BLOSUM_index_to_char ******************************************
+**
+** converts BLOSUM index number to char
+**
+** @param [r] cResType [char] index to residue type in data dir scoring matrix
+** @return [char]  original single-letter residue type
+** @@
+******************************************************************************/
+
+static ajint BLOSUM_index_to_char(ajint ajIntBlosumIndex)
+{
+    char cResType;
+
+    /* look up array for residue type order
+     *  found in EBLOSUM data files:
+     *   ARNDCQEGHILKMFPSTWYVBZX*
+     */
+    const char cArrayLookUp[28] =
+	{'A','R','N','D','C','Q','E','G','H','I','L','K','M','F','P','S','T','W','Y','V','B','Z','X','*','J','O','U','?'};
+  
+    /* XXXX SHOULD THIS BE AN OFFICIAL AMBIGUITY CODE INSTEAD? */
+    /* default to out-of-range value */
+    if ((cResType < 0) || (cResType > 27))
+	cResType = '?';
+    else
+	cResType = cArrayLookUp[ajIntBlosumIndex];
+
+    return cResType;
+}
