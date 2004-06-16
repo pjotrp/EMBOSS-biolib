237bff16fa303bb0183a905dfecb5fbaa6d46027
rice <rice>
2004-06-16 10:02:28 +0000

    need to define uchar and accept duplicate definition

--- embassy/esim4/src/types.h
+++ embassy/esim4/src/types.h
@@ -1,14 +1,15 @@
 #ifndef SIM_TYPES_H
 #define SIM_TYPES_H
-/* $Id: types.h,v 1.2 2004/06/14 14:43:30 rice Exp $ */
+/* $Id: types.h,v 1.3 2004/06/16 10:02:28 rice Exp $ */
 
 #define NACHARS 128
 
 typedef int bool;
 typedef int ss_t[NACHARS][NACHARS];
 
-/* already defined in most platforms?  - if not, how do we test? */
+/* uchar is already defined in most platforms?  - but not linux at EBI */
+/*  how do we test? */
 
-/* typedef unsigned char uchar; */
+typedef unsigned char uchar;
 
 #endif
