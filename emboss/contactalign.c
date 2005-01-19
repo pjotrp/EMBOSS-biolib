ecf035dc9d278a8803fc6f5e566fbab6958de8d8
counsell <counsell>
2005-01-19 18:10:22 +0000

    now creates new objects for modified cmap files---still doesn't write modified versions

--- emboss/contactalign.c
+++ emboss/contactalign.c
@@ -6,8 +6,8 @@
 **
 **
 ** @author: Copyright (C) Damian Counsell
-** @version $Revision: 1.28 $
-** @modified $Date: 2005/01/07 10:48:36 $
+** @version $Revision: 1.29 $
+** @modified $Date: 2005/01/19 18:10:22 $
 ** @@
 **
 ** This program is free software; you can redistribute it and/or
@@ -94,20 +94,30 @@ int main(int argc , char **argv)
     /* DDDDEBUG input contact map filename */
     AjPStr ajpStrOriginalCmapFile = NULL;
     AjPFile ajpFileOriginalCmap = NULL;
-    /* DDDDEBUG output contact map filename */
+    /* DDDDEBUG output contact maps */
     AjPStr ajpStrUpdatedCmapFile = NULL;
     AjPFile ajpFileUpdatedCmap = NULL;
+    AjPStr ajpStrSubstitutedCmapFile = NULL;
+    AjPFile ajpFileSubstitutedCmap = NULL;
+    
     
     /* contact map components */
     EmbPCmapHeader embpCmapHeader = NULL;
     AjPInt2d ajpInt2dCmapSummary = NULL;
     AjPInt2d ajpInt2dCmapResTypes = NULL;
     AjPInt2d ajpInt2dCmapPositions = NULL;
+    AjPInt2d ajpInt2dSubstitutedCmapSummary = NULL;
+    AjPInt2d ajpInt2dSubstitutedCmapResTypes = NULL;
+    AjPInt2d ajpInt2dSubstitutedCmapPositions = NULL;
 
-    /* has contact map been read successfully? */
+    /* has template contact map been read successfully? */
     AjBool ajBoolOriginalCmapFileRead = ajFalse;
-    /* has contact map been written successfully? */
+    /* has contact map been updated successfully? */
     AjBool ajBoolUpdatedCmapFileWritten = ajFalse;
+    /* has updated contact map been read successfully? */
+    AjBool ajBoolUpdatedCmapFileRead = ajFalse;
+    /* has updated contact map been substituted successfully? */
+    AjBool ajBoolSubstitutedCmapFileWritten = ajFalse;
 
     embInit("contactalign", argc, argv);
   
@@ -185,11 +195,9 @@ int main(int argc , char **argv)
 	}
     }
 
-    /* XXXX EVERYTHING FROM HERE IS TO DO WITH ALIGNMENT MODIFICATION */
-
     /* DDDD DEBUG DUMMY FILENAME BELOW */
     ajpStrOriginalCmapFile =
-	ajStrNewC("/users/damian/EMBOSS/emboss/emboss/emboss/contacttest/contacts/cmaps/d1mho__.con");
+	ajStrNewC("/users/damian/EMBOSS/emboss/emboss/emboss/SADtest/1hgx_a.con");
     ajpFileOriginalCmap = ajFileNewIn(ajpStrOriginalCmapFile);
 
     pcSeqAcross = ajSeqCharCopy(ajpSeqAcross);
@@ -216,39 +224,28 @@ int main(int argc , char **argv)
 		      pcSeqAcross,
 		      ajIntAcrossSeqLen);
     }
+
+    /* XXXX EVERYTHING FROM HERE IS TO DO WITH ALIGNMENT MODIFICATION */
+   
+    /* XXXX HARD-CODED FILENAME IS USED BELOW */
+    ajpStrUpdatedCmapFile =
+	ajStrNewC("/users/damian/EMBOSS/emboss/emboss/emboss/SADtest/test1.con");
+    ajpFileUpdatedCmap = ajFileNewOut(ajpStrUpdatedCmapFile);
     
     /* read in original contact map */ 
-    ajBoolOriginalCmapFileRead = embReadCmapFile(ajpFileOriginalCmap,
-						 ajIntAcrossSeqLen,
-						 &ajpInt2dCmapSummary,
-						 &embpCmapHeader,
-						 &ajpInt2dCmapResTypes,
-						 &ajpInt2dCmapPositions);    
+    ajBoolOriginalCmapFileRead =
+	embReadCmapFile(ajpFileOriginalCmap,
+			ajIntAcrossSeqLen,
+			&ajpInt2dCmapSummary,
+			&embpCmapHeader,
+			&ajpInt2dCmapResTypes,
+			&ajpInt2dCmapPositions);    
 
-    /* DDDDEBUGGING DID WE READ THE CMAP FILE? */
-    if( enumDebugLevel > 2 )
-    {
-	debug_cmap_header(&embpCmapHeader);
-	
-	debug_cmap_summary(&ajpInt2dCmapSummary,
-			   ajIntAcrossSeqLen);
-    }
-
-    if( enumDebugLevel > 2 )
-    {
-	debug_int_map(&ajpInt2dCmapSummary,
-		      &ajpInt2dCmapResTypes,
-		      &ajpInt2dCmapPositions,
-		      pcSeqAcross,
-		      ajIntAcrossSeqLen);
-    }
-   
-    /* DDDDEBUG: DUMMY FILENAME IS USED BELOW */
-    ajpStrUpdatedCmapFile =
-	ajStrNewC("/users/damian/EMBOSS/emboss/emboss/emboss/contacttest/contacts/cmaps/test.con");
-    ajpFileUpdatedCmap = ajFileNewOut(ajpStrUpdatedCmapFile);
-
-    /* write contact arrays to a new contact map file */
+    /*
+     * write unsubstituted contact arrays
+     *  to a new contact map file that
+     *  contains contact scores
+     */
     ajBoolUpdatedCmapFileWritten =
 	embWriteUpdatedCmapFile(ajpFileUpdatedCmap,
 				ajIntAcrossSeqLen,
@@ -256,12 +253,48 @@ int main(int argc , char **argv)
 				&embpCmapHeader,
 				&ajpInt2dCmapResTypes,
 				&ajpInt2dCmapPositions,
-				&ajpMatrixfContactScoring);
+				ajpMatrixfContactScoring);
 
-    /* XXXX LOOK UP PROBABILITY SCORE FOR PAIR */
+    /* XXXX HARD-CODED FILENAME IS USED BELOW */
+    ajpStrSubstitutedCmapFile =
+	ajStrNewC("/users/damian/EMBOSS/emboss/emboss/emboss/SADtest/test2.con");
+    ajpFileSubstitutedCmap = ajFileNewOut(ajpStrSubstitutedCmapFile);
 
-    /* convert the input float AjpMatrix to a 2D array of scores */
-    floatArrayContactScores = ajMatrixfArray(ajpMatrixfContactScoring);
+    /* reserve memory for objects representing substituted contact map */
+    ajpInt2dSubstitutedCmapSummary =
+	embGetCmapSummary(pcUpdatedSeqAcross);
+    ajpInt2dSubstitutedCmapResTypes =
+	embGetIntMap(ajIntAcrossSeqLen + enumArrayOffset);
+    ajpInt2dSubstitutedCmapPositions =
+	embGetIntMap(ajIntAcrossSeqLen + enumArrayOffset);
+    
+    /*
+     * read in updated contact map and
+     *  revise according to alignment
+     *
+     */ 
+    ajBoolUpdatedCmapFileRead =
+	embReadAndReviseCmapFile(ajpFileOriginalCmap,
+				 pcUpdatedSeqAcross,
+				 ajIntAcrossSeqLen,
+				 &ajpInt2dSubstitutedCmapSummary,
+				 &embpCmapHeader,
+				 &ajpInt2dSubstitutedCmapResTypes,
+				 &ajpInt2dSubstitutedCmapPositions);    
+
+    /*
+     * write SUBSTITUTED contact arrays
+     *  to a new contact map file that
+     *  contains contact scores
+     */
+    ajBoolSubstitutedCmapFileWritten =
+	embWriteUpdatedCmapFile(ajpFileSubstitutedCmap,
+				ajIntAcrossSeqLen,
+				&ajpInt2dCmapSummary,
+				&embpCmapHeader,
+				&ajpInt2dCmapResTypes,
+				&ajpInt2dCmapPositions,
+				ajpMatrixfContactScoring);
 
     /* XXXX FIND CORRESPONDING FIRST AND SECOND RESIDUES IN ORIGINAL TEMPLATE SEQUENCE */
 
