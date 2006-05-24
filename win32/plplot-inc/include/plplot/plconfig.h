9282b404380960bc301e25a5a6f440f2efc681b1
ajb <ajb>
2006-05-24 09:47:05 +0000

    First addition

---
+++ win32/plplot-inc/include/plplot/plconfig.h
@@ -0,0 +1,6 @@
+#ifndef __PLCONFIG_H__
+#define __PLCONFIG_H__
+#define LIB_DIR "..\\..\\..\\lib"
+#define BIN_DIR "..\\..\\..\\bin"
+#define PLPLOT_VERSION "5.1.0"
+#endif
---
+++ win32/plplot-inc/include/plplot/pldevs.h
@@ -0,0 +1,5 @@
+#define PLD_plmeta
+#define PLD_ps
+#define PLD_cps
+#define PLD_null
+#define PLD_win3
