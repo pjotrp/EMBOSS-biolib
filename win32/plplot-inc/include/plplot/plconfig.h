9acfaf1cc2cd4598a810eda1392aed5b587a81dc
ajb <ajb>
2007-05-21 15:29:09 +0000

    Directory reconstructed from plplot & plplotwin by bundlewin

--- win32/plplot-inc/include/plplot/plconfig.h
+++
@@ -1,8 +0,0 @@
-#ifndef __PLCONFIG_H__
-#define __PLCONFIG_H__
-#define LIB_DIR "..\\lib"
-#define BIN_DIR "..\\bin"
-#define DATA_DIR "..\\lib"
-#define DRV_DIR "..\\lib"
-#define PLPLT_VERSION "5.7.2e"
-#endif
--- win32/plplot-inc/include/plplot/pldevs.h
+++
@@ -1,6 +0,0 @@
-#define PLD_null
-#define PLD_plmeta
-#define PLD_ps
-#define PLD_xfig
-#define PLD_win3
-#define PLD_data
