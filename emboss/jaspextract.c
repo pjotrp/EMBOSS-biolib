1b1f77fbbf703781af9a926886bc0437c4de4c37
ajb <ajb>
2008-02-13 14:32:33 +0000

    JASPAR 2008 directory and name changes

--- emboss/jaspextract.c
+++ emboss/jaspextract.c
@@ -22,13 +22,49 @@
 
 #include "emboss.h"
 
-#define J_CORE "JASPAR_CORE"
-#define J_FAM  "JASPAR_FAM"
-#define J_PHY  "JASPAR_PHYLOFACTS"
 
 
-static AjBool jaspextract_check(const AjPStr directory, const char *sdir);
-static void   jaspextract_copy(const AjPStr directory, const char *sdir);
+
+/* @datastatic JaspDir **************************************************
+**
+** JASPAR data directory structure
+**
+** @alias JaspSDir
+** @alias JaspODir
+**
+** @attr namactual [AjPStr] name of Jaspar source subdirectory
+** @attr namdata [AjPStr] name of Jaspar subdirectory in EMBOSS_DATA area
+** @@
+******************************************************************************/
+
+typedef struct JaspSDir
+{
+    AjPStr namactual;
+    AjPStr namdata;
+} JaspODir;
+#define JaspPDir JaspODir*
+
+
+
+
+static void jaspextract_check(const AjPStr directory, AjPList dlist);
+static void jaspextract_copy(AjPList dlist);
+
+static void jaspextract_jddel(JaspPDir *thys);
+static JaspPDir jaspextract_jdnew();
+
+
+
+
+static char *jdirnames[] = {
+    "JASPAR_CORE",
+    "JASPAR_FAM",
+    "JASPAR_PHYLOFACTS",
+    "JASPAR_CNE",
+    "JASPAR_POLII",
+    "JASPAR_SPLICE",
+    NULL
+};
 
 
 
@@ -37,7 +73,7 @@ static void   jaspextract_copy(const AjPStr directory, const char *sdir);
 **
 ** Copy JASPAR database to correct EMBOSS location.
 **
-** Looks for JASPAR_CORE, JASPAR_FAM and JASPAR_PHYLOFACTS directories
+** Looks for JASPAR_* directories using prefixes in jdirnames[]
 ** and copies files to EMBOSS_DATA. Avoids usual UNIX
 ** copy commands.
 **
@@ -46,25 +82,17 @@ static void   jaspextract_copy(const AjPStr directory, const char *sdir);
 int main(int argc, char **argv)
 {
     AjPStr directory = NULL;
-    AjBool iscor = ajFalse;
-    AjBool isfam = ajFalse;
-    AjBool isphy = ajFalse;
+    AjPList dlist = NULL;
     
-
     embInit("jaspextract",argc,argv);
 
     directory = ajAcdGetDirectoryName("directory");
-    
-    iscor = jaspextract_check(directory, J_CORE);
-    isfam = jaspextract_check(directory, J_FAM);
-    isphy = jaspextract_check(directory, J_PHY);
 
-    if(iscor)
-	jaspextract_copy(directory, J_CORE);
-    if(isfam)
-	jaspextract_copy(directory, J_FAM);
-    if(isphy)
-	jaspextract_copy(directory, J_PHY);
+    dlist = ajListNew();
+    
+    jaspextract_check(directory, dlist);
+
+    jaspextract_copy(dlist);
 
     embExit();
 
@@ -76,34 +104,66 @@ int main(int argc, char **argv)
 
 /* @funcstatic jaspextract_check ***********************************************
 **
-** Checks whether a specific subdirectory exists
+** Checks whether specific JASPAR subdirectories exist
 **
 ** @param [r] directory [const AjPStr] jaspar directory
-** @param [r] sdir      [const char *] subdirectory name
+** @param [w] dlist     [AjPList] jaspar directory list
 **
-** @return [AjBool] ajTrue if the subdirectory exists
+** @return [void]
 ** @@
 ******************************************************************************/
 
-static AjBool jaspextract_check(const AjPStr directory, const char *sdir)
+static void jaspextract_check(const AjPStr directory, AjPList dlist)
 {
-    AjBool ret = ajFalse;
-    AjPStr subdir = NULL;
-
-    subdir = ajStrNew();
+    AjPStr entry  = NULL;
+    AjPStr nbase  = NULL;
     
-    ajFmtPrintS(&subdir,"%S%s",directory,sdir);
-    if(!ajFileDir(&subdir))
+    AjPList flist = NULL;
+    ajint i = 0;
+
+    JaspPDir jdir = NULL;
+    
+    
+
+    flist = ajListNew();
+
+
+    ajDirScan(directory,NULL,&flist);
+
+
+    while(ajListPop(flist,(void **)&entry))
     {
-	ajWarn("Cannot find directory %S",subdir);
-	ret = ajFalse;
+
+        if(!ajFileStat(entry,AJ_FILE_DIR))
+            continue;
+
+        ajStrAssignS(&nbase,entry);
+	ajFileDirTrim(&nbase);
+
+        i = 0;
+        while(jdirnames[i])
+        {
+            if(ajStrPrefixC(nbase,jdirnames[i]))
+            {
+                jdir = jaspextract_jdnew();
+                ajStrAssignS(&jdir->namactual,entry);
+                ajStrAssignC(&jdir->namdata,jdirnames[i]);
+                ajListPush(dlist,(void *)jdir);
+            }
+            
+
+            ++i;
+        }
+
+        ajStrDel(&entry);
     }
-    else
-	ret = ajTrue;
 
-    ajStrDel(&subdir);
 
-    return ret;
+    ajStrDel(&nbase);
+
+    ajListFree(&flist);
+    
+    return;
 }
 
 
@@ -113,16 +173,14 @@ static AjBool jaspextract_check(const AjPStr directory, const char *sdir)
 **
 ** Copy datafile into the EMBOSS  data area
 **
-** @param [r] directory [const AjPStr] jaspar directory
-** @param [r] sdir      [const char *] subdirectory name
+** @param [r] dlist [AjPList] jaspar directory list
 **
 ** @return [void]
 ** @@
 ******************************************************************************/
 
-static void jaspextract_copy(const AjPStr directory, const char *sdir)
+static void jaspextract_copy(AjPList dlist)
 {
-    AjPStr subdir = NULL;
     AjPStr wild   = NULL;
     AjPList flist = NULL;
     AjPStr entry  = NULL;
@@ -131,45 +189,56 @@ static void jaspextract_copy(const AjPStr directory, const char *sdir)
     AjPFile inf   = NULL;
     AjPFile outf  = NULL;
     AjPStr line   = NULL;
+
+    JaspPDir jdir = NULL;
     
-    subdir = ajStrNew();
+    
     wild   = ajStrNewC("*");
     flist  = ajListNew();
     nbase  = ajStrNew();
     dfile  = ajStrNew();
     line   = ajStrNew();
-    
-    ajFmtPrintS(&subdir,"%S%s",directory,sdir);
-    ajFileScan(subdir,wild,&flist,ajFalse,ajFalse,NULL,NULL,ajFalse,NULL);
 
 
-    while(ajListPop(flist,(void **)&entry))
+
+
+    while(ajListPop(dlist,(void **)&jdir))
     {
-	ajStrAssignS(&nbase,entry);
-	ajFileNameTrim(&nbase);
-	ajFmtPrintS(&dfile,"%s%c%S",sdir,SLASH_CHAR,nbase);
+        ajFileScan(jdir->namactual,wild,&flist,ajFalse,ajFalse,NULL,NULL,
+                   ajFalse,NULL);
 
-	/* Avoid UNIX copy for portability */
-	inf  = ajFileNewIn(entry);
-	if(!inf)
-	    ajFatal("Cannot open input file: %S",entry);
 
-	ajFileDataNewWrite(dfile,&outf);
-	if(!outf)
-	    ajFatal("Cannot open output file: %S",dfile);
+        while(ajListPop(flist,(void **)&entry))
+        {
+            ajStrAssignS(&nbase,entry);
+            ajFileNameTrim(&nbase);
+            ajFmtPrintS(&dfile,"%S%c%S",jdir->namdata,SLASH_CHAR,nbase);
 
-	while(ajFileReadLine(inf,&line))
-	    ajFmtPrintF(outf,"%S\n",line);
+            /* Avoid UNIX copy for portability */
+            inf  = ajFileNewIn(entry);
+            if(!inf)
+                ajFatal("Cannot open input file: %S",entry);
 
-	ajFileClose(&inf);
-	ajFileClose(&outf);
+            ajFileDataNewWrite(dfile,&outf);
+            if(!outf)
+                ajFatal("Cannot open output file: %S",dfile);
 
-	ajStrDel(&entry);
+            while(ajFileReadLine(inf,&line))
+                ajFmtPrintF(outf,"%S\n",line);
+
+            ajFileClose(&inf);
+            ajFileClose(&outf);
+
+            ajStrDel(&entry);
+        }
+
+        jaspextract_jddel(&jdir);
     }
+    
 
     ajListFree(&flist);
+    ajListFree(&dlist);
     
-    ajStrDel(&subdir);
     ajStrDel(&wild);
     ajStrDel(&dfile);
     ajStrDel(&nbase);
@@ -177,3 +246,60 @@ static void jaspextract_copy(const AjPStr directory, const char *sdir)
     
     return;
 }
+
+
+
+
+/* @funcstatic jaspextract_jdnew ***********************************************
+**
+** Create a jaspar directory structure
+**
+** @return [JaspPDir] Jaspar directory structure
+** @@
+******************************************************************************/
+
+static JaspPDir jaspextract_jdnew()
+{
+    JaspPDir ret = NULL;
+
+    AJNEW(ret);
+
+    ret->namactual = ajStrNew();
+    ret->namdata   = ajStrNew();
+
+    return ret;
+}
+
+
+
+
+/* @funcstatic jaspextract_jddel ***********************************************
+**
+** Delete a Jaspar directory structure
+**
+** @param [w] thys [JaspPDir*] jaspar directory structure
+**
+** @return [void]
+** @@
+******************************************************************************/
+
+static void jaspextract_jddel(JaspPDir *thys)
+{
+    JaspPDir pthis = NULL;
+
+    if(!thys)
+        return;
+
+    if(!*thys)
+        return;
+    
+    pthis = *thys;
+
+    ajStrDel(&pthis->namactual);
+    ajStrDel(&pthis->namdata);
+
+    *thys = NULL;
+
+    return;
+}
+
