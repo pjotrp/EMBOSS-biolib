111084a9087ba43c8010173fad29e29baca7b78a
uludag <uludag>
2008-05-09 10:10:04 +0000

    in function GraphFill checks whether graphData is null before calling the plfill function

--- ajax/ajgraph.c
+++ ajax/ajgraph.c
@@ -960,6 +960,7 @@ static void GraphFill(ajint numofpoints, float *x, float *y)
 	ajDebug("=g= plfill( %d, <> .. <>, <> .. <>) [num x..x y..y]\n",
 		numofpoints );
 
+    if (graphData != NULL)
     plfill(numofpoints, x, y);
 
     return;
