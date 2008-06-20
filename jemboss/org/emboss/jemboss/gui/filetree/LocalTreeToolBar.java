e313fbf83fcc0d31ae39a694e293fa28561ad8f7
uludag <uludag>
2008-06-20 10:30:29 +0000

    cleared a minor warning

--- jemboss/org/emboss/jemboss/gui/filetree/LocalTreeToolBar.java
+++ jemboss/org/emboss/jemboss/gui/filetree/LocalTreeToolBar.java
@@ -155,7 +155,7 @@ public class LocalTreeToolBar extends JToolBar
 //    mysettings.setUserHome(cwd);
       org.emboss.jemboss.Jemboss.tree.newRoot(cwd);
       if(SetUpMenuBar.localAndRemoteTree != null)
-      SetUpMenuBar.localAndRemoteTree.getLocalDragTree().newRoot(cwd);
+      LocalAndRemoteFileTreeFrame.getLocalDragTree().newRoot(cwd);
 
       if(!f.canWrite())
         JOptionPane.showMessageDialog(null,
