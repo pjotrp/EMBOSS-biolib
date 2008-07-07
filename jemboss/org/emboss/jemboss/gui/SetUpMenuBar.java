9b660fe55d7b55ac0abfd7da0eab1d4868b690fc
uludag <uludag>
2008-07-07 14:19:05 +0000

    showDialog call for the advanced options menu moved to AdvnacedOptions class

--- jemboss/org/emboss/jemboss/gui/SetUpMenuBar.java
+++ jemboss/org/emboss/jemboss/gui/SetUpMenuBar.java
@@ -164,27 +164,7 @@ public class SetUpMenuBar
     {
       public void actionPerformed(ActionEvent e)
       {
-        Object[] options = { "APPLY", "CANCEL" };
-
-        int select = JOptionPane.showOptionDialog(null, 
-                    ao,"Advanced Options",
-                    JOptionPane.YES_NO_CANCEL_OPTION,
-                    JOptionPane.PLAIN_MESSAGE,
-                    null,
-                    options,
-                    options[0]);
-
-        if(select == 0)
-        {
-          if(!ao.getHomeDirectory().equals(mysettings.getUserHome()))
-            mysettings.setUserHome(ao.getHomeDirectory());
-
-          if(!withSoap && !ao.getResultsDirectory().equals(mysettings.getResultsHome()))
-            mysettings.setResultsHome(ao.getResultsDirectory()); 
-        }
-//      JOptionPane jao = new JOptionPane();
-//      jao.showMessageDialog(f,ao,"Advanced Options",
-//                            JOptionPane.PLAIN_MESSAGE);
+        ao.showDiaolog(mysettings, withSoap, f, f.getX()+70, f.getY()+50);
       }
     });
     prefsMenu.add(showAdvOpt);
@@ -452,8 +432,8 @@ public class SetUpMenuBar
     writeFavorites();
     String fs = new String(System.getProperty("file.separator"));
     String cwd = new String(System.getProperty("user.dir") + fs);
-    if(ao.isSaveUserHomeSelected())
-      ao.userHomeSave();
+
+    ao.userHomeSave();
 
     if(seqList.isStoreSequenceList())  //create a SequenceList file
       saveSequenceList();
