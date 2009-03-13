f652389956515013f034c4763ba37f25b7a17f2f
rice <rice>
2009-03-13 16:30:36 +0000

    use SO term for polypeptide_motif for report

--- emboss/patmatdb.c
+++ emboss/patmatdb.c
@@ -66,7 +66,7 @@ int main(int argc, char **argv)
 
     temp=ajStrNew();
     ajStrFmtUpper(&motif);
-    ajStrAssignC(&fthit, "hit");
+    ajStrAssignC(&fthit, "SO:0001067");
 
     /*converting the Prosite motif to a reg exps */
     regexp =embPatPrositeToRegExp(motif);
