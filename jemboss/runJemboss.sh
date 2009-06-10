18365b4b45f203f11867adad705be53da728bacb
uludag <uludag>
2009-06-10 15:51:43 +0000

    - deleted xerces.jar from java CLASSPATH, it looks like this library is not needed Java 1.4 forwards - also deleted JNI related LD_LIBRARY_PATH settings, as they are not needed any more

--- jemboss/runJemboss.sh
+++ jemboss/runJemboss.sh
@@ -10,7 +10,6 @@ export LIB
 CLASSPATH=${JEMBOSS_HOME}:$LIB/JembossPrintAlignment.jar
 CLASSPATH=${CLASSPATH}:$LIB/jakarta-regexp-1.2.jar
 CLASSPATH=${CLASSPATH}:$LIB/jalview.jar
-CLASSPATH=${CLASSPATH}:$LIB/xerces.jar
 CLASSPATH=${CLASSPATH}:$LIB/mail.jar
 CLASSPATH=${CLASSPATH}:$LIB/activation.jar
 CLASSPATH=${CLASSPATH}:$LIB/axis/axis.jar
@@ -31,19 +30,7 @@ export CLASSPATH
 #setenv CLASSPATH ${CLASSPATH}:$LIB/axis/commons-logging.jar:$LIB/axis/commons-discovery.jar
 #setenv CLASSPATH ${CLASSPATH}:$LIB/grout.jar:$LIB/client.jar:$LIB/jalview.jar:.
 #
-#Commented vars are required for standalone use
-#
-#EMBOSS_INSTALL=/packages/emboss/STABLE/lib; export EMBOSS_INSTALL
-#LD_LIBRARY_PATH=$EMBOSS_INSTALL; export LD_LIBRARY_PATH
-#
-#For MacOSX:
-#DYLD_LIBRARY_PATH=$EMBOSS_INSTALL; export DYLD_LIBRARY_PATH
-#
-#For HPUX:
-#SHLIB_PATH=$EMBOSS_INSTALL; export SHLIB_PATH
-#
-#For AIX
-#LIBPATH=/usr/lib/threads:/usr/lib:/lib:$EMBOSS_INSTALL; export LIBPATH
+
 #
 #Add local to run as 'standalone':
 #java org.emboss.jemboss.Jemboss local &
