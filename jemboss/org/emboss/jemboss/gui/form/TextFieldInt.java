e4260335387194bfa1c8b80ee2a6ea7a48f789c6
uludag <uludag>
2009-07-02 14:56:16 +0000

    better error message (actually we might not need this message at all?)

--- jemboss/org/emboss/jemboss/gui/form/TextFieldInt.java
+++ jemboss/org/emboss/jemboss/gui/form/TextFieldInt.java
@@ -94,7 +94,7 @@ public class TextFieldInt extends TextFieldSink
           if(source[i] != ',')
           {
             toolkit.beep();
-            System.err.println("insertString: " + source[i]);
+            System.err.println("invalid character entered: " + source[i]);
           }
         }
       }
