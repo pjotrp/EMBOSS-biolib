38f6660d88d14cd28945681b7f3c70ca8bd99e85
uludag <uludag>
2009-07-28 08:45:17 +0000

    Get the version of EMBOSS programs used by the Jemboss server connected

---
+++ jemboss/org/emboss/jemboss/soap/GetVersion.java
@@ -0,0 +1,82 @@
+/********************************************************************
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Library General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Library General Public
+ *  License along with this library; if not, write to the
+ *  Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ *  Boston, MA  02111-1307, USA.
+ *
+ *  @author: Copyright (C) Tim Carver
+ *
+ ********************************************************************/
+
+package org.emboss.jemboss.soap;
+
+import javax.xml.namespace.QName;
+
+import org.apache.axis.client.Call;
+import org.emboss.jemboss.JembossParams;
+
+/**
+ * 
+ * Get the version of EMBOSS programs used by the Jemboss server connected
+ * 
+ */
+/**
+ * @author uludag
+ *
+ */
+public class GetVersion {
+
+	private static String version = null;
+
+	/**
+	 * returns complete version string of EMBOSS connected Jemboss server is using
+	 * @param mysettings
+	 *            jemboss properties
+	 * 
+	 */
+	public static String getVersionComplete(JembossParams mysettings) {
+
+		if (version != null)
+			return version;
+
+		if (!JembossParams.isJembossServer())
+			return null;
+		try {
+			String endpoint = mysettings.getPublicSoapURL();
+			org.apache.axis.client.Service serv = new org.apache.axis.client.Service();
+
+			Call call = (Call) serv.createCall();
+			call.setTargetEndpointAddress(new java.net.URL(endpoint));
+			call.setOperationName(new QName(mysettings.getPublicSoapService(),
+					"version"));
+			call.setReturnType(org.apache.axis.Constants.SOAP_STRING);
+			version = (String) call.invoke(new Object[] {});
+		} catch (Exception jse) {
+			jse.printStackTrace();
+		}
+		return version;
+	}
+	
+	
+	/**
+	 * returns firt two digits of EMBOSS version connected server has
+	 * @param mysettings
+	 * @return version of EMBOSS programs such as 5.0, or 6.1
+	 */
+	public static String getVersion(JembossParams mysettings){
+		getVersionComplete(mysettings);
+		return version.substring(0,version.lastIndexOf('.'));        
+	}
+
+}
