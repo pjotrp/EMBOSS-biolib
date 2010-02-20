9fdd4a7a9d5439c676d5bdbcf0988ab08e009efd
mks <mks>
2010-02-20 23:45:24 +0000

    * Renamed Ensembl Meta Coordinate Adaptor variables from the generic adaptor to   the slightly more specific mca. * Continued EMBOSS-style reformatting. * Comments in CPP #endif lines indicate the matching CPP #if statement.

---
+++ emboss/ajax/ensembl/ensmetacoordinate.h
@@ -0,0 +1,71 @@
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#ifndef ensmetacoordinate_h
+#define ensmetacoordinate_h
+
+#include "enstable.h"
+#include "enscoordsystem.h"
+
+
+
+
+/* @data EnsPMetacoordinateadaptor ********************************************
+**
+** Ensembl Meta-Coordinate Adaptor.
+**
+** @alias EnsSMetacoordinateadaptor
+** @alias EnsOMetacoordinateadaptor
+**
+** @attr Adaptor [EnsPDatabaseadaptor] Ensembl Database Adaptor.
+** @attr CacheByName [AjPTable] Ensembl Feature name (i.e. table name) cache.
+** @attr CacheByLength [AjPTable] Maximum length cache.
+** @@
+******************************************************************************/
+
+typedef struct EnsSMetacoordinateadaptor
+{
+    EnsPDatabaseadaptor Adaptor;
+    AjPTable CacheByName;
+    AjPTable CacheByLength;
+} EnsOMetacoordinateadaptor;
+
+#define EnsPMetacoordinateadaptor EnsOMetacoordinateadaptor*
+
+
+
+
+/*
+** Prototype definitions
+*/
+
+/* Ensembl Meta-Coordinate Adaptor */
+
+EnsPMetacoordinateadaptor ensMetacoordinateadaptorNew(EnsPDatabaseadaptor dba);
+
+void ensMetacoordinateadaptorDel(EnsPMetacoordinateadaptor *Pmca);
+
+AjBool ensMetacoordinateadaptorFetchAllCoordsystems(
+    EnsPMetacoordinateadaptor mca,
+    const AjPStr name,
+    AjPList css);
+
+ajuint ensMetacoordinateadaptorGetMaximumLength(
+    EnsPMetacoordinateadaptor mca,
+    const EnsPCoordsystem cs,
+    const AjPStr name);
+
+/*
+** End of prototype definitions
+*/
+
+
+
+
+#endif /* ensmetacoordinate_h */
+
+#ifdef __cplusplus
+}
+#endif
