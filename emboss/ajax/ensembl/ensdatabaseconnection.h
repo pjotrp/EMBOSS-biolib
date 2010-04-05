304823c9678306f8f61ee96c371ea607f4309174
mks <mks>
2010-04-05 15:12:37 +0000

    * Changed variable name 'socket' to 'socketfile' as it clashes with a global   declaration in <sys/socket.h>. * Changed the Socket element of the Ensembl Database Connection to SocketFile. * Continued EMBOSS-style reformatting. * Comments after CPP #endif statements indicate the matching CPP #if statement.

---
+++ emboss/ajax/ensembl/ensdatabaseconnection.h
@@ -0,0 +1,125 @@
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#ifndef ensdatabaseconnection_h
+#define ensdatabaseconnection_h
+
+#include "ajax.h"
+
+
+
+
+/* @data EnsPDatabaseconnection ***********************************************
+**
+** Ensembl Database Connection.
+**
+** Holds information to connect to a particular SQL database on a
+** relational database management system (RDBMS) instance.
+**
+** @alias EnsSDatabaseconnection
+** @alias EnsODatabaseconnection
+**
+** @attr Sqlconnection [AjPSqlconnection] AJAX SQL Connection
+** @attr UserName [AjPStr] User name
+** @attr Password [AjPStr] Password
+** @attr HostName [AjPStr] Host name or IP address
+** @attr HostPort [AjPStr] Host TCP/IP port
+** @attr SocketFile [AjPStr] UNIX socket file
+** @attr DatabaseName [AjPStr] SQL database name
+** @attr SqlClientType [AjEnum] AJAX SQL client type
+** @attr Use [ajuint] Use counter
+**
+** @@
+******************************************************************************/
+
+typedef struct EnsSDatabaseconnection
+{
+    AjPSqlconnection Sqlconnection;
+    AjPStr UserName;
+    AjPStr Password;
+    AjPStr HostName;
+    AjPStr HostPort;
+    AjPStr SocketFile;
+    AjPStr DatabaseName;
+    AjEnum SqlClientType;
+    ajuint Use;
+} EnsODatabaseconnection;
+
+#define EnsPDatabaseconnection EnsODatabaseconnection*
+
+
+
+
+/*
+** Prototype definitions
+*/
+
+EnsPDatabaseconnection ensDatabaseconnectionNew(AjEnum client,
+                                                AjPStr user,
+                                                AjPStr password,
+                                                AjPStr host,
+                                                AjPStr port,
+                                                AjPStr socketfile,
+                                                AjPStr database);
+
+EnsPDatabaseconnection ensDatabaseconnectionNewC(EnsPDatabaseconnection dbc,
+                                                 AjPStr database);
+
+EnsPDatabaseconnection ensDatabaseconnectionNewRef(EnsPDatabaseconnection dbc);
+
+void ensDatabaseconnectionDel(EnsPDatabaseconnection* Pdbc);
+
+AjPSqlconnection ensDatabaseconnectionGetSqlconnection(
+    const EnsPDatabaseconnection dbc);
+
+AjPStr ensDatabaseconnectionGetUserName(const EnsPDatabaseconnection dbc);
+
+AjPStr ensDatabaseconnectionGetPassword(const EnsPDatabaseconnection dbc);
+
+AjPStr ensDatabaseconnectionGetHostName(const EnsPDatabaseconnection dbc);
+
+AjPStr ensDatabaseconnectionGetHostPort(const EnsPDatabaseconnection dbc);
+
+AjPStr ensDatabaseconnectionGetSocketFile(const EnsPDatabaseconnection dbc);
+
+AjPStr ensDatabaseconnectionGetDatabaseName(const EnsPDatabaseconnection dbc);
+
+AjEnum ensDatabaseconnectionGetSqlClientType(const EnsPDatabaseconnection dbc);
+
+AjBool ensDatabaseconnectionMatch(const EnsPDatabaseconnection dbc1,
+                                  const EnsPDatabaseconnection dbc2);
+
+AjBool ensDatabaseconnectionConnect(EnsPDatabaseconnection dbc);
+
+void ensDatabaseconnectionDisconnect(EnsPDatabaseconnection dbc);
+
+AjBool ensDatabaseconnectionIsConnected(const EnsPDatabaseconnection dbc);
+
+AjPSqlstatement ensDatabaseconnectionSqlstatementNew(EnsPDatabaseconnection dbc,
+                                                     const AjPStr statement);
+
+AjBool ensDatabaseconnectionEscapeC(EnsPDatabaseconnection dbc,
+                                    char **Ptxt,
+                                    const AjPStr str);
+
+AjBool ensDatabaseconnectionEscapeS(EnsPDatabaseconnection dbc,
+                                    AjPStr *Pstr,
+                                    const AjPStr str);
+
+AjBool ensDatabaseconnectionTrace(const EnsPDatabaseconnection dbc,
+                                  ajuint level);
+
+/*
+** End of prototype definitions
+*/
+
+
+
+
+#endif /* ensdatabaseconnection_h */
+
+#ifdef __cplusplus
+}
+#endif
