77aecfd94ab16551757ed2ec69ae0dd0c9b9805c
rice <rice>
2010-05-14 13:46:16 +0000

    PDF and SVG sizes as defined in plplot

--- emboss/ajax/graphics/ajgraph.c
+++ emboss/ajax/graphics/ajgraph.c
@@ -320,12 +320,12 @@ static GraphOType graphType[] =
 #endif
 #endif
 
-    {"pdf",        "pdf",     ".pdf",   800,  600,
+    {"pdf",        "pdf",     ".pdf",  1683, 1190,
      AJTRUE,  AJFALSE, AJFALSE, AJFALSE, AJFALSE,  AJFALSE,
      GraphxyDisplayToFile, GraphOpenFile,
      "Adobe PDF"},
 
-    {"svg",        "svg",     ".svg",   800,  600,
+    {"svg",        "svg",     ".svg",   720,  540, /* see plplot/svg.c */
      AJTRUE,  AJFALSE, AJFALSE, AJFALSE, AJFALSE,  AJFALSE,
      GraphxyDisplayToFile, GraphOpenFile,
      "Scalable vector graphics"},
