1ddab4d4fb023b238308746d6257d93a65fe3fd9
mks <mks>
2010-07-06 11:53:25 +0000

    * New functions:   * ensSeqinTrace   * ensSeqQueryTrace   * ensSeqDescTrace   * ensSeqTrace

--- emboss/ajax/ensembl/ensutility.h
+++ emboss/ajax/ensembl/ensutility.h
@@ -21,6 +21,14 @@ void ensInit(void);
 
 void ensExit(void);
 
+AjBool ensSeqinTrace(const AjPSeqin seqin, ajuint level);
+
+AjBool ensSeqQueryTrace(const AjPSeqQuery seqquery, ajuint level);
+
+AjBool ensSeqDescTrace(const AjPSeqDesc seqdesc, ajuint level);
+
+AjBool ensSeqTrace(const AjPSeq seq, ajuint level);
+
 /*
 ** End of prototype definitions
 */
