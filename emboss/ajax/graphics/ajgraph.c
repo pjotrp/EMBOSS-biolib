6bc14285f88c2f2e004319f6e59439da19e939df
rice <rice>
2010-07-19 10:05:15 +0000

    graphics device sized pdf and svg

--- emboss/ajax/graphics/ajgraph.c
+++ emboss/ajax/graphics/ajgraph.c
@@ -328,13 +328,13 @@ static GraphOType graphType[] =
 #endif
 
 #ifdef PLD_pdf          /* if png/gd/zlib libraries available for png driver */
-    {"pdf",        "pdf",     ".pdf",   800,  600,
+    {"pdf",        "pdf",     ".pdf",   1683,  1190,
      AJTRUE,  AJFALSE, AJFALSE, AJFALSE, AJFALSE,  AJFALSE,
      GraphxyDisplayToFile, GraphOpenFile,
      "PDF graphics files"},
 #endif
 
-    {"svg",        "svg",     ".svg",   800,  600,
+    {"svg",        "svg",     ".svg",   720,  540,
      AJTRUE,  AJFALSE, AJFALSE, AJFALSE, AJFALSE,  AJFALSE,
      GraphxyDisplayToFile, GraphOpenFile,
      "SVG graphics files"},
