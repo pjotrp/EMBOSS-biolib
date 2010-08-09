f17a799f649a289a1414ebf06e3e6eece30b468d
mks <mks>
2010-08-09 12:29:12 +0000

    * Fixed the file header block, which was not a proper C comment.

--- emboss/ajax/core/template.c
+++ emboss/ajax/core/template.c
@@ -1,3 +1,4 @@
+/******************************************************************************
 ** @source __AJAX or NUCLEUS__  __LibraryArea__ functions
 **
 ** __ShortDescription__
@@ -20,7 +21,7 @@
 ** You should have received a copy of the GNU General Public License
 ** along with this program; if not, write to the Free Software
 ** Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-********************************************************************/
+******************************************************************************/
 
 /* ==================================================================== */
 /* ========================== include files =========================== */
